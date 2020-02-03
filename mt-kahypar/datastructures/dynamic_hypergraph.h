5040758a69685656d1590067c16be9020975a81f
Tobias Heuer <tobias.heuer@gmx.net>
2020-02-03 13:10:51 +0100

    add layout classes for hypergraph refactoring

---
+++ mt-kahypar/datastructures/dynamic_hypergraph.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * This file is part of KaHyPar.
+ *
+ * Copyright (C) 2019 Tobias Heuer <tobias.heuer@kit.edu>
+ *
+ * KaHyPar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaHyPar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaHyPar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#include "kahypar/meta/mandatory.h"
+
+namespace mt_kahypar {
+namespace ds {
+
+template <typename TBBNumaArena = Mandatory>
+class DynamicHypergraph {
+
+ public:
+  static constexpr bool is_static_hypergraph = false;
+  static constexpr bool is_partitioned = false;
+
+  explicit DynamicHypergraph() { }
+
+  DynamicHypergraph(const DynamicHypergraph&) = delete;
+  DynamicHypergraph & operator= (const DynamicHypergraph &) = delete;
+};
+
+} // namespace ds
+} // namespace mt_kahypar
---
+++ mt-kahypar/datastructures/numa_hypergraph.h
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * This file is part of KaHyPar.
+ *
+ * Copyright (C) 2019 Tobias Heuer <tobias.heuer@kit.edu>
+ *
+ * KaHyPar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaHyPar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaHyPar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#include <type_traits>
+
+#include "kahypar/meta/mandatory.h"
+
+namespace mt_kahypar {
+namespace ds {
+
+template <typename Hypergraph = Mandatory,
+          typename HardwareTopology = Mandatory,
+          typename TBBNumaArena = Mandatory>
+class NumaHypergraph {
+
+  static_assert(!Hypergraph::is_partitioned,  "Only unpartitioned hypergraphs are allowed");
+
+ public:
+  static constexpr bool is_static_hypergraph = Hypergraph::is_static_hypergraph;
+  static constexpr bool is_partitioned = false;
+
+  explicit NumaHypergraph() { }
+
+  NumaHypergraph(const NumaHypergraph&) = delete;
+  NumaHypergraph & operator= (const NumaHypergraph &) = delete;
+};
+
+} // namespace ds
+} // namespace mt_kahypar
