1f3e107374d1179b4e970abc011814cf03412bb0
Tobias Heuer <tobias.heuer@gmx.net>
2020-02-11 11:17:28 +0100

    forward function to wrapped hypergraph in partitioned hypergraph

--- mt-kahypar/datastructures/partitioned_hypergraph.h
+++ mt-kahypar/datastructures/partitioned_hypergraph.h
@@ -23,6 +23,7 @@
 #include "kahypar/meta/mandatory.h"
 
 #include "mt-kahypar/datastructures/hypergraph_common.h"
+#include "mt-kahypar/utils/range.h"
 
 namespace mt_kahypar {
 namespace ds {
@@ -31,7 +32,19 @@ template <typename Hypergraph = Mandatory,
           typename TBBNumaArena = Mandatory>
 class PartitionedHypergraph {
 
-  static_assert(!Hypergraph::is_numa_aware,  "Only non-numa-aware hypergraphs are allowed");
+  static_assert(!Hypergraph::is_partitioned,  "Only non-numa-aware hypergraphs are allowed");
+  static_assert(!Hypergraph::is_numa_aware,  "Only unpartitioned hypergraphs are allowed");
+
+  // ! Iterator to iterate over the hypernodes
+  using HypernodeIterator = typename Hypergraph::HypernodeIterator;
+  // ! Iterator to iterate over the hyperedges
+  using HyperedgeIterator = typename Hypergraph::HyperedgeIterator;
+  // ! Iterator to iterate over the pins of a hyperedge
+  using IncidenceIterator = typename Hypergraph::IncidenceIterator;
+  // ! Iterator to iterate over the incident nets of a hypernode
+  using IncidentNetsIterator = typename Hypergraph::IncidentNetsIterator;
+  // ! Iterator to iterate over the set of communities contained in a hyperedge
+  using CommunityIterator = typename Hypergraph::CommunityIterator;
 
  public:
   static constexpr bool is_static_hypergraph = Hypergraph::is_static_hypergraph;
@@ -43,6 +56,274 @@ class PartitionedHypergraph {
 
   PartitionedHypergraph(const PartitionedHypergraph&) = delete;
   PartitionedHypergraph & operator= (const PartitionedHypergraph &) = delete;
+
+  // ####################### General Hypergraph Stats #######################
+
+  // ! Number of NUMA hypergraphs
+  size_t numNumaHypergraphs() const {
+    return _hg.numNumaHypergraphs();
+  }
+
+  // ! Initial number of hypernodes
+  HypernodeID initialNumNodes() const {
+    return _hg.initialNumNodes();
+  }
+
+  // ! Initial number of hypernodes on numa node
+  HypernodeID initialNumNodes(const int node) const {
+    return _hg.initialNumNodes(node);
+  }
+
+  // ! Number of removed hypernodes
+  HypernodeID numRemovedHypernodes() const {
+    return _hg.numRemovedHypernodes();
+  }
+
+  // ! Initial number of hyperedges
+  HyperedgeID initialNumEdges() const {
+    return _hg.initialNumEdges();
+  }
+
+  // ! Initial number of hyperedges on numa node
+  HyperedgeID initialNumEdges(const int node) const {
+    return _hg.initialNumEdges(node);
+  }
+
+  // ! Initial number of pins
+  HypernodeID initialNumPins() const {
+    return _hg.initialNumPins();
+  }
+
+  // ! Initial number of pins on numa node
+  HypernodeID initialNumPins(const int node) const {
+    return _hg.initialNumPins(node);
+  }
+
+  // ! Initial sum of the degree of all vertices
+  HypernodeID initialTotalVertexDegree() const {
+    return _hg.initialTotalVertexDegree();
+  }
+
+  // ! Initial sum of the degree of all vertices on numa node
+  HypernodeID initialTotalVertexDegree(const int node) const {
+    return _hg.initialTotalVertexDegree(node);
+  }
+
+  // ! Total weight of hypergraph
+  HypernodeWeight totalWeight() const {
+    return _hg.totalWeight();
+  }
+
+  // ####################### Iterators #######################
+
+  // ! Iterates in parallel over all active nodes and calls function f
+  // ! for each vertex
+  template<typename F>
+  void doParallelForAllNodes(const TaskGroupID task_group_id, const F& f) {
+    static_cast<const PartitionedHypergraph&>(*this).doParallelForAllNodes(task_group_id, f);
+  }
+
+  // ! Iterates in parallel over all active nodes and calls function f
+  // ! for each vertex
+  template<typename F>
+  void doParallelForAllNodes(const TaskGroupID task_group_id, const F& f) const {
+    _hg.doParallelForAllNodes(task_group_id, f);
+  }
+
+  // ! Iterates in parallel over all active edges and calls function f
+  // ! for each net
+  template<typename F>
+  void doParallelForAllEdges(const TaskGroupID task_group_id, const F& f) {
+    static_cast<const PartitionedHypergraph&>(*this).doParallelForAllEdges(task_group_id, f);
+  }
+
+  // ! Iterates in parallel over all active edges and calls function f
+  // ! for each net
+  template<typename F>
+  void doParallelForAllEdges(const TaskGroupID task_group_id, const F& f) const {
+    _hg.doParallelForAllEdges(task_group_id, f);
+  }
+
+  // ! Returns an iterator over the set of active nodes of the hypergraph
+  ConcatenatedRange<IteratorRange<HypernodeIterator>> nodes() const {
+    return _hg.nodes();
+  }
+
+  // ! Returns an iterator over the set of active nodes of the hypergraph on a numa node
+  IteratorRange<HypernodeIterator> nodes(const int node) const {
+    return _hg.nodes(node);
+  }
+
+  // ! Returns an iterator over the set of active edges of the hypergraph
+  ConcatenatedRange<IteratorRange<HyperedgeIterator>> edges() const {
+    return _hg.edges();
+  }
+
+  // ! Returns an iterator over the set of active nodes of the hypergraph on a numa node
+  IteratorRange<HyperedgeIterator> edges(const int node) const {
+    return _hg.edges();
+  }
+
+  // ! Returns a range to loop over the incident nets of hypernode u.
+  IteratorRange<IncidentNetsIterator> incidentEdges(const HypernodeID u) const {
+    return _hg.incidentEdges(u);
+  }
+
+  // ! Returns a range to loop over the pins of hyperedge e.
+  IteratorRange<IncidenceIterator> pins(const HyperedgeID e) const {
+    return hypergraph_of_edge(e).pins(e);
+  }
+
+  // ####################### Hypernode Information #######################
+
+  // ! Returns for a vertex of the hypergraph its original vertex id
+  // ! Can be used to map the global vertex ids to a consecutive range
+  // ! of nodes between [0,|V|).
+  HypernodeID originalNodeID(const HypernodeID u) const {
+    return _hg.originalNodeID(u);
+  }
+
+  // ! Reverse operation of originalNodeID(u)
+  HypernodeID globalNodeID(const HypernodeID u) const {
+    return _hg.globalNodeID(u);
+  }
+
+  // ! Weight of a vertex
+  HypernodeWeight nodeWeight(const HypernodeID u) const {
+    return _hg.nodeWeight(u);
+  }
+
+  // ! Sets the weight of a vertex
+  void setNodeWeight(const HypernodeID u, const HypernodeWeight weight) {
+    _hg.setNodeWeight(u, weight);
+  }
+
+
+  // ! Degree of a hypernode
+  HyperedgeID nodeDegree(const HypernodeID u) const {
+    return _hg.nodeDegree(u);
+  }
+
+  // ! Returns, if the corresponding vertex is high degree vertex
+  bool isHighDegreeVertex(const HypernodeID u) const {
+    return _hg.isHighDegreeVertex(u);
+  }
+
+  // ! Returns, whether a hypernode is enabled or not
+  bool nodeIsEnabled(const HypernodeID u) const {
+    return _hg.nodeIsEnabled(u);
+  }
+
+  // ! Enables a hypernode (must be disabled before)
+  void enableHypernode(const HypernodeID u) {
+    _hg.enableHypernode(u);
+  }
+
+  // ! Disable a hypernode (must be enabled before)
+  void disableHypernode(const HypernodeID u) {
+    _hg.disableHypernode(u);
+  }
+
+  // ####################### Hyperedge Information #######################
+
+  // ! Returns for a edge of the hypergraph its original edge id
+  // ! Can be used to map the global edge ids to a consecutive range
+  // ! of edges between [0,|E|).
+  HypernodeID originalEdgeID(const HyperedgeID e) const {
+    return _hg.originalEdgeID(e);
+  }
+
+  // ! Reverse operation of originalEdgeID(e)
+  HypernodeID globalEdgeID(const HyperedgeID e) const {
+    return _hg.globalEdgeID(e);
+  }
+
+  // ! Weight of a hyperedge
+  HypernodeWeight edgeWeight(const HyperedgeID e) const {
+    return _hg.edgeWeight(e);
+  }
+
+  // ! Sets the weight of a hyperedge
+  void setEdgeWeight(const HyperedgeID e, const HyperedgeWeight weight) {
+    _hg.setEdgeWeight(e, weight);
+  }
+
+  // ! Number of pins of a hyperedge
+  HypernodeID edgeSize(const HyperedgeID e) const {
+    return _hg.edgeSize(e);
+  }
+
+  // ! Returns, whether a hyperedge is enabled or not
+  bool edgeIsEnabled(const HyperedgeID e) const {
+    return _hg.edgeIsEnabled(e);
+  }
+
+  // ! Enables a hyperedge (must be disabled before)
+  void enableHyperedge(const HyperedgeID e) {
+    _hg.enableHyperedge(e);
+  }
+
+  // ! Disabled a hyperedge (must be enabled before)
+  void disableHyperedge(const HyperedgeID e) {
+    _hg.disableHyperedge(e);
+  }
+
+  // ####################### Uncontraction #######################
+
+  void uncontract(const Memento&, parallel::scalable_vector<HyperedgeID>&) {
+    ERROR("uncontract(memento,parallel_he) is not supported in partitioned hypergraph");
+  }
+
+  void uncontract(const std::vector<Memento>&,
+                  parallel::scalable_vector<HyperedgeID>&,
+                  const kahypar::ds::FastResetFlagArray<>&,
+                  const bool) {
+    ERROR("uncontract(...) is not supported in partitioned hypergraph");
+  }
+
+  void restoreDisabledHyperedgesThatBecomeNonParallel(
+    const Memento&,
+    parallel::scalable_vector<HyperedgeID>&,
+    const kahypar::ds::FastResetFlagArray<>&) {
+    ERROR("restoreDisabledHyperedgesThatBecomeNonParallel(...) is not supported"
+          << "in partitioned hypergraph");
+  }
+
+  parallel::scalable_vector<HyperedgeID> findDisabledHyperedgesThatBecomeNonParallel(
+    const Memento&,
+    parallel::scalable_vector<HyperedgeID>&,
+    const kahypar::ds::FastResetFlagArray<>&) {
+    ERROR("findDisabledHyperedgesThatBecomeNonParallel(...) is not supported"
+          << "in partitioned hypergraph");
+    return parallel::scalable_vector<HyperedgeID>();
+  }
+
+  // ####################### Restore Hyperedges #######################
+
+  // ! Restores an hyperedge of a certain size.
+  void restoreEdge(const HyperedgeID he, const size_t size,
+                   const HyperedgeID representative = kInvalidHyperedge) {
+    _hg.restoreEdge(he, size, representative);
+  }
+
+  // ! Restores a single-pin hyperedge
+  void restoreSinglePinHyperedge(const HyperedgeID he) {
+    _hg.restoreSinglePinHyperedge(he);
+  }
+
+  void restoreParallelHyperedge(const HyperedgeID,
+                                const Memento&,
+                                parallel::scalable_vector<HyperedgeID>&,
+                                const kahypar::ds::FastResetFlagArray<>* batch_hypernodes = nullptr) {
+    unused(batch_hypernodes);
+    ERROR("restoreParallelHyperedge(...) is not supported in partitioned hypergraph");
+  }
+
+  // ####################### Memory Consumption #######################
+
+  void memoryConsumption(utils::MemoryTreeNode* parent) const {
+    ASSERT(parent);
+  }
 
  private:
   Hypergraph& _hg;
