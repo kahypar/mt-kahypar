632e917082d20a9fc53b15b0de4257af606cad45
Tobias Heuer <tobias.heuer@gmx.net>
2020-02-24 17:12:24 +0100

    fix unit test

--- mt-kahypar/io/sql_plottools_serializer.h
+++ mt-kahypar/io/sql_plottools_serializer.h
@@ -87,7 +87,7 @@ static inline std::string serialize(const PartitionedHypergraph<>& hypergraph,
         << " initial_partitioning_lp_initial_block_size=" << context.initial_partitioning.lp_initial_block_size
         << " initial_partitioning_use_heavy_hyperedge_removal=" << std::boolalpha << context.initial_partitioning.use_heavy_hyperedge_removal
         << " initial_partitioning_hyperedge_pin_weight_fraction=" << context.initial_partitioning.hyperedge_pin_weight_fraction
-        << " initial_partitioning_max_hyperedge_pin_weight_fraction=" << context.initial_partitioning.max_hyperedge_pin_weight
+        << " initial_partitioning_max_hyperedge_pin_weight=" << context.initial_partitioning.max_hyperedge_pin_weight
         << " use_batch_uncontractions=" << std::boolalpha << context.refinement.use_batch_uncontractions
         << " batch_size=" << context.refinement.batch_size
         << " lp_algorithm=" << context.refinement.label_propagation.algorithm
