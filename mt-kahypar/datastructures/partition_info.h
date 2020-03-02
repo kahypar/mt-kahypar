763c1fa9292034336cd5cff46b4eb1414c22681d
Tobias Heuer <tobias.heuer@gmx.net>
2020-03-02 10:25:01 +0100

    add partition info object to manage block weights and sizes in scalable way

---
+++ mt-kahypar/datastructures/partition_info.h
@@ -0,0 +1,171 @@
+/*******************************************************************************
+ * This file is part of KaHyPar.
+ *
+ * Copyright (C) 2019 Tobias Heuer <tobias.heuer@kit.edu>
+ *
+ * KaHyPar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaHyPar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaHyPar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+
+#include <cmath>
+
+#include "mt-kahypar/macros.h"
+#include "mt-kahypar/datastructures/hypergraph_common.h"
+#include "mt-kahypar/parallel/atomic_wrapper.h"
+#include "mt-kahypar/parallel/stl/scalable_vector.h"
+
+namespace mt_kahypar {
+namespace ds {
+
+/*!
+ * Stores for each block of the partition its size and weight.
+ * Internally, we store for each block ceil(log2(p)/log2(k)) local block
+ * info objects, where p is the number of processors and k is the number
+ * of blocks we want to partition the hypergraph into. A block info
+ * objects stores the block size and weight as an atomic. To determine
+ * the block weight or size, we have to sum up over the local block info
+ * objects for the corresponding block.
+ * The idea of using log2(p)/log2(k) local block info objects is to prevent
+ * congestion when performing atomic updates of block weights and sizes concurrently
+ * compared to an variant where we only use one atomic for the weights and sizes.
+ * Furthermore, by using only a logarithmic number of local block infos, we
+ * still can guarantee * reasonable fast queries to get the global part
+ * weights and sizes.
+ * If k is small compared to the number of threads, than the workload
+ * is scattered onto a logarithmic number of local block infos. If k
+ * is equal or even greater than number of threads, we use only one
+ * local part info object for each block, which also represents than
+ * the global block weight and size.
+ */
+class PartitionInfo {
+
+  using AtomicWeight = parallel::IntegralAtomicWrapper<HypernodeWeight>;
+  using AtomicSize = parallel::IntegralAtomicWrapper<HypernodeID>;
+
+ public:
+  /*!
+   * For each block \f$V_i\f$ of the \f$k\f$-way partition
+   * \f$\mathrm{\Pi} = \{V_1, \dots, V_k\}\f$, a BlockInfo object stores
+   * the number of hypernodes currently assigned to block \f$V_i\f$
+   * as well as the sum of their weights.
+   */
+  class BlockInfo {
+   public:
+    explicit BlockInfo() :
+      weight(0),
+      size(0) { }
+
+    bool operator== (const BlockInfo& other) const {
+      return weight == other.weight && size == other.size;
+    }
+
+    AtomicWeight weight;
+    AtomicSize size;
+  };
+
+ private:
+  // For one block, we store several BlockInfo object in order
+  // to minimize congestion on the atomics.
+  using LocalBlockInfo = parallel::scalable_vector<BlockInfo>;
+
+ public:
+  PartitionInfo() :
+    _k(0),
+    _num_local_block_infos(0),
+    _part_info() { }
+
+  PartitionInfo(const size_t num_threads, const PartitionID k) :
+    _k(k),
+    _num_local_block_infos(computeNumLocalBlockInfos(num_threads, k)),
+    _part_info(k, LocalBlockInfo(computeNumLocalBlockInfos(num_threads, k))) {
+    ASSERT(_num_local_block_infos > 0);
+  }
+
+  PartitionInfo(const PartitionInfo&) = delete;
+  PartitionInfo & operator= (const PartitionInfo &) = delete;
+
+  HypernodeID numLocalBlockInfos() const {
+    return _num_local_block_infos;
+  }
+
+  inline void setBlockSize(const PartitionID id,
+                           const HypernodeID size) {
+    ASSERT(id != kInvalidPartition && id < _k);
+    _part_info[id][0].size = size;
+    ASSERT(partSize(id) == size);
+  }
+
+  inline void setBlockWeight(const PartitionID id,
+                             const HypernodeWeight weight) {
+    ASSERT(id != kInvalidPartition && id < _k);
+    _part_info[id][0].weight = weight;
+    ASSERT(partWeight(id) == weight);
+  }
+
+
+  inline void setNodePart(const HypernodeID u,
+                          const PartitionID id,
+                          const HypernodeWeight weight) {
+    ASSERT(id != kInvalidPartition && id < _k);
+    const HypernodeID local_bucket = u % _num_local_block_infos;
+    ++_part_info[id][local_bucket].size;
+    _part_info[id][local_bucket].weight += weight;
+  }
+
+  inline void changeNodePart(const HypernodeID u,
+                             const PartitionID from,
+                             const PartitionID to,
+                             const HypernodeWeight weight) {
+    ASSERT(from != kInvalidPartition && from < _k);
+    ASSERT(to != kInvalidPartition && to < _k);
+    ASSERT(from != to);
+    const HypernodeID local_bucket = u % _num_local_block_infos;
+    --_part_info[from][local_bucket].size;
+    _part_info[from][local_bucket].weight -= weight;
+    ++_part_info[to][local_bucket].size;
+    _part_info[to][local_bucket].weight += weight;
+  }
+
+  inline HypernodeWeight partWeight(const PartitionID id) const {
+    ASSERT(id != kInvalidPartition && id < _k);
+    HypernodeWeight weight = 0;
+    for ( size_t i = 0; i < _num_local_block_infos; ++i ) {
+      weight += _part_info[id][i].weight;
+    }
+    return weight;
+  }
+
+  inline HypernodeID partSize(const PartitionID id) const {
+    ASSERT(id != kInvalidPartition && id < _k);
+    HypernodeID size = 0;
+    for ( size_t i = 0; i < _num_local_block_infos; ++i ) {
+      size += _part_info[id][i].size;
+    }
+    return size;
+  }
+
+ private:
+  HypernodeID computeNumLocalBlockInfos(const size_t num_threads, const PartitionID k) {
+    const double log2_num_threads = std::log2(static_cast<double>(num_threads));
+    const double log2_k = std::log2(static_cast<double>(k));
+    return std::ceil(log2_num_threads / log2_k);
+  }
+
+  PartitionID _k;
+  HypernodeID _num_local_block_infos;
+  parallel::scalable_vector<LocalBlockInfo> _part_info;
+};
+}  // namespace ds
+}  // namespace mt_kahypar
---
+++ tests/datastructures/partition_info_test.cc
@@ -0,0 +1,242 @@
+/*******************************************************************************
+ * This file is part of KaHyPar.
+ *
+ * Copyright (C) 2019 Tobias Heuer <tobias.heuer@kit.edu>
+ *
+ * KaHyPar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaHyPar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaHyPar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#include <atomic>
+
+#include "gmock/gmock.h"
+
+#include "mt-kahypar/datastructures/partition_info.h"
+#include "mt-kahypar/utils/randomize.h"
+
+using ::testing::Test;
+
+namespace mt_kahypar {
+namespace ds {
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos1) {
+  PartitionInfo part_info(8, 8);
+  ASSERT_EQ(1, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos2) {
+  PartitionInfo part_info(8, 2);
+  ASSERT_EQ(3, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos3) {
+  PartitionInfo part_info(8, 4);
+  ASSERT_EQ(2, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos4) {
+  PartitionInfo part_info(16, 4);
+  ASSERT_EQ(2, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos5) {
+  PartitionInfo part_info(64, 2);
+  ASSERT_EQ(6, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos6) {
+  PartitionInfo part_info(64, 4);
+  ASSERT_EQ(3, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos7) {
+  PartitionInfo part_info(64, 8);
+  ASSERT_EQ(2, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos8) {
+  PartitionInfo part_info(64, 16);
+  ASSERT_EQ(2, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectNumLocalBlockInfos9) {
+  PartitionInfo part_info(64, 64);
+  ASSERT_EQ(1, part_info.numLocalBlockInfos());
+}
+
+TEST(APartitionInfo, HasCorrectBlockSizeIfSetNodePartIsCalled1) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  ASSERT_EQ(1, part_info.partSize(2));
+}
+
+TEST(APartitionInfo, HasCorrectBlockWeightIfSetNodePartIsCalled1) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  ASSERT_EQ(3, part_info.partWeight(2));
+}
+
+TEST(APartitionInfo, HasCorrectBlockSizeIfSetNodePartIsCalled2) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 3, 2);
+  ASSERT_EQ(1, part_info.partSize(2));
+  ASSERT_EQ(1, part_info.partSize(3));
+}
+
+TEST(APartitionInfo, HasCorrectBlockWeightIfSetNodePartIsCalled2) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 3, 2);
+  ASSERT_EQ(3, part_info.partWeight(2));
+  ASSERT_EQ(2, part_info.partWeight(3));
+}
+
+TEST(APartitionInfo, HasCorrectBlockSizeIfSetNodePartIsCalled3) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 2, 2);
+  ASSERT_EQ(2, part_info.partSize(2));
+}
+
+TEST(APartitionInfo, HasCorrectBlockWeightIfSetNodePartIsCalled3) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 2, 2);
+  ASSERT_EQ(5, part_info.partWeight(2));
+}
+
+TEST(APartitionInfo, HasCorrectBlockSizeIfChangeNodePartIsCalled1) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 2, 2);
+  part_info.changeNodePart(1, 2, 0, 2);
+  ASSERT_EQ(1, part_info.partSize(2));
+  ASSERT_EQ(1, part_info.partSize(0));
+}
+
+TEST(APartitionInfo, HasCorrectBlockWeightIfChangeNodePartIsCalled1) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 2, 2);
+  part_info.changeNodePart(1, 2, 0, 2);
+  ASSERT_EQ(3, part_info.partWeight(2));
+  ASSERT_EQ(2, part_info.partWeight(0));
+}
+
+TEST(APartitionInfo, HasCorrectBlockSizeIfChangeNodePartIsCalled2) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 2, 2);
+  part_info.changeNodePart(1, 2, 0, 2);
+  part_info.changeNodePart(0, 2, 1, 3);
+  ASSERT_EQ(0, part_info.partSize(2));
+  ASSERT_EQ(1, part_info.partSize(1));
+  ASSERT_EQ(1, part_info.partSize(0));
+}
+
+TEST(APartitionInfo, HasCorrectBlockWeightIfChangeNodePartIsCalled2) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 2, 2);
+  part_info.changeNodePart(1, 2, 0, 2);
+  part_info.changeNodePart(0, 2, 1, 3);
+  ASSERT_EQ(0, part_info.partWeight(2));
+  ASSERT_EQ(3, part_info.partWeight(1));
+  ASSERT_EQ(2, part_info.partWeight(0));
+}
+
+TEST(APartitionInfo, HasCorrectBlockSizeIfChangeNodePartIsCalled3) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 2, 2);
+  part_info.changeNodePart(1, 2, 0, 2);
+  part_info.changeNodePart(0, 2, 0, 3);
+  ASSERT_EQ(0, part_info.partSize(2));
+  ASSERT_EQ(2, part_info.partSize(0));
+}
+
+TEST(APartitionInfo, HasCorrectBlockWeightIfChangeNodePartIsCalled3) {
+  PartitionInfo part_info(64, 4);
+  part_info.setNodePart(0, 2, 3);
+  part_info.setNodePart(1, 2, 2);
+  part_info.changeNodePart(1, 2, 0, 2);
+  part_info.changeNodePart(0, 2, 0, 3);
+  ASSERT_EQ(0, part_info.partWeight(2));
+  ASSERT_EQ(5, part_info.partWeight(0));
+}
+
+using AtomicWeight = parallel::IntegralAtomicWrapper<HypernodeWeight>;
+using AtomicSize = parallel::IntegralAtomicWrapper<HypernodeID>;
+
+void smokeTest(const HypernodeID num_hypernodes,
+               const size_t num_threads,
+               const PartitionID k) {
+  std::vector<HypernodeWeight> weights(num_hypernodes, 0);
+  for ( HypernodeID hn = 0; hn < num_hypernodes; ++hn ) {
+    weights[hn] = utils::Randomize::instance().getRandomInt(1, 10, sched_getcpu());
+  }
+
+  PartitionInfo part_info(num_threads, k);
+  std::vector<PartitionID> part_id(num_hypernodes, kInvalidPartition);
+  std::vector<AtomicSize> part_size(k, AtomicSize(0));
+  std::vector<AtomicWeight> part_weight(k, AtomicWeight(0));
+
+  // Set Vertex Partition IDs
+  tbb::parallel_for(ID(0), num_hypernodes, [&](const HypernodeID hn) {
+    const PartitionID block = utils::Randomize::instance().getRandomInt(0, k-1, sched_getcpu());
+    part_id[hn] = block;
+    ++part_size[block];
+    part_weight[block] += weights[hn];
+    part_info.setNodePart(hn, block, weights[hn]);
+  });
+
+  for ( PartitionID block = 0; block < k; ++block ) {
+    ASSERT_EQ(part_size[block].load(), part_info.partSize(block));
+    ASSERT_EQ(part_weight[block].load(), part_info.partWeight(block));
+  }
+
+  // Change Vertex Partition IDs
+  tbb::parallel_for(ID(0), num_hypernodes, [&](const HypernodeID hn) {
+    const PartitionID from = part_id[hn];
+    const PartitionID to = utils::Randomize::instance().getRandomInt(0, k-1, sched_getcpu());
+    if ( from != to ) {
+      --part_size[from];
+      part_weight[from] -= weights[hn];
+      ++part_size[to];
+      part_weight[to] += weights[hn];
+      part_info.changeNodePart(hn, from, to, weights[hn]);
+    }
+  });
+
+  for ( PartitionID block = 0; block < k; ++block ) {
+    ASSERT_EQ(part_size[block].load(), part_info.partSize(block));
+    ASSERT_EQ(part_weight[block].load(), part_info.partWeight(block));
+  }
+}
+
+TEST(APartitionInfo, SmokeTest1) {
+  smokeTest(ID(100000), 64, 4);
+}
+
+TEST(APartitionInfo, SmokeTest2) {
+  smokeTest(ID(100000), 64, 16);
+}
+
+TEST(APartitionInfo, SmokeTest3) {
+  smokeTest(ID(100000), 64, 64);
+}
+
+} // namespace
+} // namespace
