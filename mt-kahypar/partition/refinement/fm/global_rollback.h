a5c0074e4df78029c48e7d9532d8d5ee374409a8
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-05-04 22:29:33 +0200

    remove more empty lines

--- mt-kahypar/partition/refinement/fm/global_rollback.h
+++ mt-kahypar/partition/refinement/fm/global_rollback.h
@@ -172,8 +172,6 @@ public:
     utils::Timer& timer = utils::Timer::instance();
     timer.start_timer("move_id_flagging", "Move Flagging");
 
-
-
     tbb::parallel_for(0U, numMoves, [&](MoveID localMoveID) {
       const Move& m = move_order[localMoveID];
       if (!sharedData.moveTracker.isMoveStillValid(m)) return;
