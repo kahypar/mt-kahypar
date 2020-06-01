316305c0fe7223ff60cfd61cd28edd483202cc72
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-06-01 14:06:03 +0200

    serialize new option. and add to presets

--- mt-kahypar/io/sql_plottools_serializer.h
+++ mt-kahypar/io/sql_plottools_serializer.h
@@ -101,6 +101,7 @@ static inline std::string serialize(const PartitionedHypergraph& hypergraph,
         << " fm_perform_moves_global=" << std::boolalpha << context.refinement.fm.perform_moves_global
         << " fm_revert_parallel=" << std::boolalpha << context.refinement.fm.revert_parallel
         << " fm_rollback_balance_violation_factor" << context.refinement.fm.rollback_balance_violation_factor
+        << " fm_min_improvement" << context.refinement.fm.min_improvement
         << " fm_num_seed_nodes=" << context.refinement.fm.num_seed_nodes
         << " fm_shuffle=" << std::boolalpha << context.refinement.fm.shuffle
         << " fm_obey_minimal_parallelism=" << std::boolalpha << context.refinement.fm.obey_minimal_parallelism
