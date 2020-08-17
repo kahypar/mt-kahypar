ae6aebe8b177a8079c62d4b0ccbbba8db20f0e3c
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-08-17 20:20:11 +0200

    extract label propagation functions into extra cpp

--- mt-kahypar/partition/refinement/label_propagation/label_propagation_refiner.h
+++ mt-kahypar/partition/refinement/label_propagation/label_propagation_refiner.h
@@ -74,101 +74,11 @@ class LabelPropagationRefiner final : public IRefiner {
   LabelPropagationRefiner & operator= (LabelPropagationRefiner &&) = delete;
 
  private:
-  bool refineImpl(PartitionedHypergraph& hypergraph,
-                  kahypar::Metrics& best_metrics,
-                  const double) override final {
-    _gain.reset();
-    _next_active.reset();
+  bool refineImpl(PartitionedHypergraph& hypergraph, kahypar::Metrics& best_metrics, double) final ;
 
-    // Perform Label Propagation
-    labelPropagation(hypergraph);
+  void labelPropagation(PartitionedHypergraph& hypergraph);
 
-    // Update global part weight and sizes
-    best_metrics.imbalance = metrics::imbalance(hypergraph, _context);
-
-    // Update metrics statistics
-    HyperedgeWeight current_metric = best_metrics.getMetric(
-      kahypar::Mode::direct_kway, _context.partition.objective);
-    Gain delta = _gain.delta();
-    ASSERT(delta <= 0, "LP refiner worsen solution quality");
-
-    HEAVY_REFINEMENT_ASSERT(current_metric + delta ==
-                            metrics::objective(hypergraph, _context.partition.objective,
-                                               !_context.refinement.label_propagation.execute_sequential),
-                            V(current_metric) << V(delta) <<
-                            V(metrics::objective(hypergraph, _context.partition.objective,
-                                                 _context.refinement.label_propagation.execute_sequential)));
-
-    best_metrics.updateMetric(current_metric + delta, kahypar::Mode::direct_kway, _context.partition.objective);
-    utils::Stats::instance().update_stat("lp_improvement", std::abs(delta));
-    return delta < 0;
-  }
-
-  void labelPropagation(PartitionedHypergraph& hypergraph) {
-    NextActiveNodes next_active_nodes;
-    for (size_t i = 0; i < _context.refinement.label_propagation.maximum_iterations; ++i) {
-      DBG << "Starting Label Propagation Round" << i;
-
-      utils::Timer::instance().start_timer(
-        "lp_round_" + std::to_string(i), "Label Propagation Round " + std::to_string(i), true);
-
-      if ( _active_nodes.size() > 0 ) {
-        labelPropagationRound(hypergraph, next_active_nodes);
-      }
-
-      if ( _context.refinement.label_propagation.execute_sequential ) {
-        _active_nodes = next_active_nodes.copy_sequential();
-        next_active_nodes.clear_sequential();
-      } else {
-        _active_nodes = next_active_nodes.copy_parallel();
-        next_active_nodes.clear_parallel();
-      }
-      utils::Timer::instance().stop_timer("lp_round_" + std::to_string(i));
-
-      if ( _active_nodes.size() == 0 ) {
-        break;
-      }
-    }
-  }
-
-  bool labelPropagationRound(PartitionedHypergraph& hypergraph,
-                             NextActiveNodes& next_active_nodes) {
-    _visited_he.reset();
-    _next_active.reset();
-    // This function is passed as lambda to the changeNodePart function and used
-    // to calculate the "real" delta of a move (in terms of the used objective function).
-    auto objective_delta = [&](const HyperedgeID he,
-                               const HyperedgeWeight edge_weight,
-                               const HypernodeID edge_size,
-                               const HypernodeID pin_count_in_from_part_after,
-                               const HypernodeID pin_count_in_to_part_after) {
-                             _gain.computeDeltaForHyperedge(he, edge_weight, edge_size,
-                                                            pin_count_in_from_part_after, pin_count_in_to_part_after);
-                           };
-
-    // Shuffle Vector
-    bool converged = true;
-    if ( _context.refinement.label_propagation.execute_sequential ) {
-      utils::Randomize::instance().shuffleVector(
-        _active_nodes, 0UL, _active_nodes.size(), sched_getcpu());
-
-      for ( size_t j = 0; j < _active_nodes.size(); ++j ) {
-        const HypernodeID hn = _active_nodes[j];
-        converged &= !moveVertex(hypergraph, hn,
-          next_active_nodes, objective_delta);
-      }
-    } else {
-      utils::Randomize::instance().parallelShuffleVector(
-        _active_nodes, 0UL, _active_nodes.size());
-
-      tbb::parallel_for(0UL, _active_nodes.size(), [&](const size_t& j) {
-          const HypernodeID hn = _active_nodes[j];
-          converged &= !moveVertex(hypergraph, hn,
-            next_active_nodes, objective_delta);
-        });
-    }
-    return converged;
-  }
+  bool labelPropagationRound(PartitionedHypergraph& hypergraph, NextActiveNodes& next_active_nodes);
 
   template<typename F>
   bool moveVertex(PartitionedHypergraph& hypergraph,
@@ -241,37 +151,7 @@ class LabelPropagationRefiner final : public IRefiner {
     return is_moved;
   }
 
-  void initializeImpl(PartitionedHypergraph& hypergraph) override final {
-    ActiveNodes tmp_active_nodes;
-    _active_nodes = std::move(tmp_active_nodes);
-
-    if ( _context.refinement.label_propagation.execute_sequential ) {
-      // Setup active nodes sequential
-      for ( const HypernodeID hn : hypergraph.nodes() ) {
-        if ( _context.refinement.label_propagation.rebalancing ||
-             hypergraph.isBorderNode(hn) ) {
-          _active_nodes.push_back(hn);
-        }
-      }
-    } else {
-      // Setup active nodes in parallel
-      // A node is active, if it is a border vertex.
-      NextActiveNodes tmp_active_nodes;
-
-      auto add_vertex = [&](const HypernodeID& hn) {
-        tmp_active_nodes.stream(hn);
-      };
-
-      hypergraph.doParallelForAllNodes([&](const HypernodeID& hn) {
-        if ( _context.refinement.label_propagation.rebalancing ||
-             hypergraph.isBorderNode(hn) ) {
-          add_vertex(hn);
-        }
-      });
-
-      _active_nodes = tmp_active_nodes.copy_parallel();
-    }
-  }
+  void initializeImpl(PartitionedHypergraph& hypergraph) final ;
 
   const Context& _context;
   const TaskGroupID _task_group_id;
