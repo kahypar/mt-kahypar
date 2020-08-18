131746dfb09bad48ce356b122a5a4b80e1e76e0a
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-08-18 16:09:02 +0200

    add coarsener tests

--- tests/partition/coarsening/coarsener_fixtures.h
+++ tests/partition/coarsening/coarsener_fixtures.h
@@ -47,7 +47,6 @@ class ACoarsener : public Test {
     for ( const HypernodeID& hn : hypergraph.nodes() ) {
       hypergraph.setCommunityID(hn, hn / 4);
     }
-    hypergraph.initializeCommunities();
 
     context.partition.k = 2;
     context.partition.mode = kahypar::Mode::direct_kway;
