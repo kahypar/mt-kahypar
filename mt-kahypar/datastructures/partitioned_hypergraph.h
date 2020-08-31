9961d4a6a352d259bb6957b1d90ac4171879f6c4
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-08-31 20:41:51 +0200

    remove break

--- mt-kahypar/datastructures/partitioned_hypergraph.h
+++ mt-kahypar/datastructures/partitioned_hypergraph.h
@@ -1022,7 +1022,6 @@ private:
         nodeGainAssertions(u, from);
         if (partID(u) == from) {
           _move_from_benefit[u].fetch_add(we, std::memory_order_relaxed);
-          break;
         }
       }
     } else if (pin_count_in_from_part_after == 0) {
