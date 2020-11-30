ce7439be38bad3574f4d1ed63b7fcb99e82b5d89
Tobias Heuer <tobias.heuer@gmx.net>
2020-11-30 11:10:39 +0100

    fix reinsertion of large hyperedges in vcycle

--- mt-kahypar/partition/partitioner.cpp
+++ mt-kahypar/partition/partitioner.cpp
@@ -128,7 +128,8 @@ namespace mt_kahypar {
 
   PartitionedHypergraph partitionVCycle(Hypergraph& hypergraph,
                                         PartitionedHypergraph&& partitioned_hypergraph,
-                                        Context& context) {
+                                        Context& context,
+                                        LargeHyperedgeRemover& large_he_remover) {
     ASSERT(context.partition.num_vcycles > 0);
 
     for ( size_t i = 0; i < context.partition.num_vcycles; ++i ) {
@@ -136,6 +137,12 @@ namespace mt_kahypar {
       hypergraph.reset();
       parallel::MemoryPool::instance().reset();
       parallel::MemoryPool::instance().release_mem_group("Preprocessing");
+
+      if ( context.partition.paradigm == Paradigm::nlevel ) {
+        // Workaround: reset() function of hypergraph reinserts all removed
+        // hyperedges to incident net lists of each vertex again.
+        large_he_remover.removeLargeHyperedgesInNLevelVCycle(hypergraph);
+      }
 
       // Store partition and assign it as community ids in order to
       // restrict contractions in v-cycle to partition ids
@@ -176,7 +183,9 @@ namespace mt_kahypar {
 
     // ################## V-Cycle s##################
     if ( context.partition.num_vcycles > 0 ) {
-      partitioned_hypergraph = partitionVCycle(hypergraph, std::move(partitioned_hypergraph), context);
+      partitioned_hypergraph = partitionVCycle(
+        hypergraph, std::move(partitioned_hypergraph),
+        context, large_he_remover);
     }
 
     // ################## POSTPROCESSING ##################
--- mt-kahypar/partition/preprocessing/sparsification/large_he_remover.h
+++ mt-kahypar/partition/preprocessing/sparsification/large_he_remover.h
@@ -57,6 +57,17 @@ class LargeHyperedgeRemover {
     return num_removed_large_hyperedges;
   }
 
+  // ! Before we start a v-cycle, we reset the hypergraph data structure.
+  // ! This causes that all removed hyperedges in the dynamic hypergraph are
+  // ! reinserted to the incident nets of each vertex. By simply calling this
+  // ! function, we remove all large hyperedges again.
+  void removeLargeHyperedgesInNLevelVCycle(Hypergraph& hypergraph) {
+    for ( const HyperedgeID& he : _removed_hes ) {
+      hypergraph.enableHyperedge(he);
+      hypergraph.removeLargeEdge(he);
+    }
+  }
+
   // ! Restores all previously removed large hyperedges
   void restoreLargeHyperedges(PartitionedHypergraph& hypergraph) {
     HyperedgeWeight delta = 0;
