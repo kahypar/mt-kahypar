cac27d62d25d79d6251c8f237444d29d054a7657
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2021-02-01 14:48:31 +0100

    add bench shuffle tool

---
+++ tools/bench_deterministic_shuffling.cpp
@@ -0,0 +1,59 @@
+#include "mt-kahypar/utils/reproducible_random.h"
+
+#include <iostream>
+#include <random>
+#include <tbb/task_scheduler_init.h>
+
+namespace mt_kahypar::utils {
+
+
+void benchShuffle(size_t n, int num_threads) {
+  tbb::task_scheduler_init tsi(num_threads);
+
+#ifndef NDEBUG
+  auto is_permutation = [&](vec<int>& r1, vec<int>& r2) {
+    std::sort(r1.begin(), r1.end());
+    std::sort(r2.begin(), r2.end());
+    return r1 == r2;
+  };
+#endif
+
+  ParallelPermutation<int, PrecomputeBucket> shuffle_preassign;
+  ParallelPermutation<int, PrecomputeBucketOpt> shuffle_preassign_opt;
+  ParallelPermutation<int, BucketHashing> shuffle_hash;
+
+  uint32_t seed = 420;
+  std::mt19937 rng(seed);
+
+  vec<int> comp(n);
+  std::iota(comp.begin(), comp.end(), 0);
+
+  std::cout << "preassign buckets" << std::endl;
+  shuffle_preassign.create_integer_permutation(n, num_threads, rng);
+  assert(is_permutation(comp, shuffle_preassign.permutation));
+
+  std::cout << "preassign buckets opt" << std::endl;
+  rng.seed(seed);
+  shuffle_preassign_opt.create_integer_permutation(n, num_threads, rng);
+  assert(is_permutation(comp, shuffle_preassign_opt.permutation));
+
+  std::cout << "hash" << std::endl;
+  rng.seed(seed);
+  shuffle_hash.create_integer_permutation(n, num_threads, rng);
+  assert(is_permutation(comp, shuffle_hash.permutation));
+}
+
+}
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 3) {
+    std::cout << "Usage. num-threads permutation-size" << std::endl;
+    std::exit(0);
+  }
+
+  int num_threads = std::stoi(argv[1]);
+  size_t n = std::stoi(argv[2]);
+  mt_kahypar::utils::benchShuffle(n, num_threads);
+  return 0;
+}
