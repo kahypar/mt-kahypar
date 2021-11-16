9a6a31b7041edec23b98e2ac45dab834edf8715e
Tobias Heuer <tobias.heuer@gmx.net>
2021-11-16 17:49:35 +0100

    fix max part weights sizes for flow problem

--- mt-kahypar/partition/refinement/advanced/problem_construction.cpp
+++ mt-kahypar/partition/refinement/advanced/problem_construction.cpp
@@ -100,9 +100,9 @@ Subhypergraph ProblemConstruction::construct(const SearchID search_id,
   sub_hg.weight_of_block_1 = 0;
   sub_hg.num_pins = 0;
   const HypernodeWeight max_weight_block_0 =
-    _scaling * _context.partition.perfect_balance_part_weights[sub_hg.block_0] - phg.partWeight(sub_hg.block_0);
-  const HypernodeWeight max_weight_block_1 =
     _scaling * _context.partition.perfect_balance_part_weights[sub_hg.block_1] - phg.partWeight(sub_hg.block_1);
+  const HypernodeWeight max_weight_block_1 =
+    _scaling * _context.partition.perfect_balance_part_weights[sub_hg.block_0] - phg.partWeight(sub_hg.block_0);
   const size_t max_bfs_distance = _context.refinement.advanced.max_bfs_distance;
 
 
