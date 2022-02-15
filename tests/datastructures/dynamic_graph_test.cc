4f574d32f8a79cd2f97f2aaebff985d3a42c4421
Nikolai Maas <nikolai.maas@student.kit.edu>
2022-02-15 14:43:56 +0100

    more tests

--- tests/datastructures/dynamic_graph_test.cc
+++ tests/datastructures/dynamic_graph_test.cc
@@ -148,6 +148,12 @@ TEST_F(ADynamicGraph, VerifiesPinsOfEdges) {
     { {1, 2}, {1, 4}, {2, 3}, {4, 5}, {4, 6}, {5, 6} });
 }
 
+TEST_F(ADynamicGraph, VerifiesVertexWeights) {
+  for ( const HypernodeID& hn : hypergraph.nodes() ) {
+    ASSERT_EQ(1, hypergraph.nodeWeight(hn));
+  }
+}
+
 TEST_F(ADynamicGraph, HasCorrectEdgeIteratorIfVerticesAreDisabled) {
   hypergraph.disableHypernode(5);
   hypergraph.disableHypernode(6);
@@ -158,6 +164,124 @@ TEST_F(ADynamicGraph, HasCorrectEdgeIteratorIfVerticesAreDisabled) {
     ASSERT_EQ(expected_iter[pos++], he);
   }
   ASSERT_EQ(expected_iter.size(), pos);
+}
+
+TEST_F(ADynamicGraph, IteratesParallelOverAllNodes) {
+  std::vector<uint8_t> visited(7, false);
+  hypergraph.doParallelForAllNodes([&](const HypernodeID hn) {
+      visited[hn] = true;
+    });
+
+  for ( size_t i = 0; i < visited.size(); ++i ) {
+    ASSERT_TRUE(visited[i]) << i;
+  }
+}
+
+TEST_F(ADynamicGraph, ModifiesNodeWeight) {
+  hypergraph.setNodeWeight(0, 2);
+  hypergraph.setNodeWeight(6, 2);
+  ASSERT_EQ(2, hypergraph.nodeWeight(0));
+  ASSERT_EQ(2, hypergraph.nodeWeight(6));
+  hypergraph.updateTotalWeight(parallel_tag_t());
+  ASSERT_EQ(9, hypergraph.totalWeight());
+}
+
+
+TEST_F(ADynamicGraph, VerifiesVertexDegrees) {
+  ASSERT_EQ(0, hypergraph.nodeDegree(0));
+  ASSERT_EQ(2, hypergraph.nodeDegree(1));
+  ASSERT_EQ(2, hypergraph.nodeDegree(2));
+  ASSERT_EQ(1, hypergraph.nodeDegree(3));
+  ASSERT_EQ(3, hypergraph.nodeDegree(4));
+  ASSERT_EQ(2, hypergraph.nodeDegree(5));
+  ASSERT_EQ(2, hypergraph.nodeDegree(6));
+}
+
+TEST_F(ADynamicGraph, RemovesVertices) {
+  hypergraph.removeHypernode(0);
+  hypergraph.removeHypernode(5);
+  ASSERT_EQ(2, hypergraph.numRemovedHypernodes());
+}
+
+TEST_F(ADynamicGraph, VerifiesEdgeWeights) {
+  for ( const HyperedgeID& he : hypergraph.edges() ) {
+    ASSERT_EQ(1, hypergraph.edgeWeight(he));
+  }
+}
+
+TEST_F(ADynamicGraph, ModifiesEdgeWeight) {
+  hypergraph.setEdgeWeight(0, 2);
+  hypergraph.setEdgeWeight(2, 2);
+  ASSERT_EQ(2, hypergraph.edgeWeight(0));
+  ASSERT_EQ(2, hypergraph.edgeWeight(2));
+}
+
+TEST_F(ADynamicGraph, VerifiesEdgeSizes) {
+  for ( const HyperedgeID& he : hypergraph.edges() ) {
+    ASSERT_EQ(2, hypergraph.edgeSize(he));
+  }
+}
+
+TEST_F(ADynamicGraph, SetsCommunityIDsForEachVertex) {
+  hypergraph.setCommunityID(0, 1);
+  hypergraph.setCommunityID(1, 1);
+  hypergraph.setCommunityID(2, 1);
+  hypergraph.setCommunityID(3, 2);
+  hypergraph.setCommunityID(4, 2);
+  hypergraph.setCommunityID(5, 3);
+  hypergraph.setCommunityID(6, 3);
+
+  ASSERT_EQ(1, hypergraph.communityID(0));
+  ASSERT_EQ(1, hypergraph.communityID(1));
+  ASSERT_EQ(1, hypergraph.communityID(2));
+  ASSERT_EQ(2, hypergraph.communityID(3));
+  ASSERT_EQ(2, hypergraph.communityID(4));
+  ASSERT_EQ(3, hypergraph.communityID(5));
+  ASSERT_EQ(3, hypergraph.communityID(6));
+}
+
+TEST_F(ADynamicGraph, ComparesStatsIfCopiedParallel) {
+  DynamicGraph copy_hg = hypergraph.copy(parallel_tag_t());
+  ASSERT_EQ(hypergraph.initialNumNodes(), copy_hg.initialNumNodes());
+  ASSERT_EQ(hypergraph.initialNumEdges(), copy_hg.initialNumEdges());
+  ASSERT_EQ(hypergraph.initialNumPins(), copy_hg.initialNumPins());
+  ASSERT_EQ(hypergraph.initialTotalVertexDegree(), copy_hg.initialTotalVertexDegree());
+  ASSERT_EQ(hypergraph.totalWeight(), copy_hg.totalWeight());
+  ASSERT_EQ(hypergraph.maxEdgeSize(), copy_hg.maxEdgeSize());
+}
+
+TEST_F(ADynamicGraph, ComparesStatsIfCopiedSequential) {
+  DynamicGraph copy_hg = hypergraph.copy();
+  ASSERT_EQ(hypergraph.initialNumNodes(), copy_hg.initialNumNodes());
+  ASSERT_EQ(hypergraph.initialNumEdges(), copy_hg.initialNumEdges());
+  ASSERT_EQ(hypergraph.initialNumPins(), copy_hg.initialNumPins());
+  ASSERT_EQ(hypergraph.initialTotalVertexDegree(), copy_hg.initialTotalVertexDegree());
+  ASSERT_EQ(hypergraph.totalWeight(), copy_hg.totalWeight());
+  ASSERT_EQ(hypergraph.maxEdgeSize(), copy_hg.maxEdgeSize());
+}
+
+TEST_F(ADynamicGraph, ComparesIncidentEdgesIfCopiedParallel) {
+  DynamicGraph copy_hg = hypergraph.copy(parallel_tag_t());
+  auto edges = expected_edges();
+  verifyIncidentNets(copy_hg, 0, { });
+  verifyIncidentNets(copy_hg, 1, { edges[0], edges[1] });
+  verifyIncidentNets(copy_hg, 2, { edges[2], edges[3] });
+  verifyIncidentNets(copy_hg, 3, { edges[4] });
+  verifyIncidentNets(copy_hg, 4, { edges[5], edges[6], edges[7] });
+  verifyIncidentNets(copy_hg, 5, { edges[8], edges[9] });
+  verifyIncidentNets(copy_hg, 6, { edges[10], edges[11] });
+}
+
+TEST_F(ADynamicGraph, ComparesIncidentEdgesIfCopiedSequential) {
+  DynamicGraph copy_hg = hypergraph.copy();
+  auto edges = expected_edges();
+  verifyIncidentNets(copy_hg, 0, { });
+  verifyIncidentNets(copy_hg, 1, { edges[0], edges[1] });
+  verifyIncidentNets(copy_hg, 2, { edges[2], edges[3] });
+  verifyIncidentNets(copy_hg, 3, { edges[4] });
+  verifyIncidentNets(copy_hg, 4, { edges[5], edges[6], edges[7] });
+  verifyIncidentNets(copy_hg, 5, { edges[8], edges[9] });
+  verifyIncidentNets(copy_hg, 6, { edges[10], edges[11] });
 }
 
 } // namespace ds
