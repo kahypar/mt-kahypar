032b6cfdeec74e89f3edadf917bc4ea793ade6a5
Nikolai Maas <nikolai.maas@student.kit.edu>
2022-03-09 12:31:15 +0100

    formatting fix

--- mt-kahypar/datastructures/dynamic_graph_factory.cpp
+++ mt-kahypar/datastructures/dynamic_graph_factory.cpp
@@ -30,68 +30,69 @@
 #include "mt-kahypar/utils/timer.h"
 
 namespace mt_kahypar::ds {
-  DynamicGraph DynamicGraphFactory::construct(
-          const HypernodeID num_nodes,
-          const HyperedgeID num_edges,
-          const HyperedgeVector& edge_vector,
-          const HyperedgeWeight* edge_weight,
-          const HypernodeWeight* node_weight,
-          const bool stable_construction_of_incident_edges) {
-    ASSERT(edge_vector.size() == num_edges);
 
-    EdgeVector edges;
-    edges.reserve(num_edges);
-    for (const auto& e : edge_vector) {
-      if (e.size() != 2) {
-        ERROR("Using graph data structure; but the input hypergraph is not a graph.");
-      }
-      edges.push_back({e[0], e[1]});
+DynamicGraph DynamicGraphFactory::construct(
+        const HypernodeID num_nodes,
+        const HyperedgeID num_edges,
+        const HyperedgeVector& edge_vector,
+        const HyperedgeWeight* edge_weight,
+        const HypernodeWeight* node_weight,
+        const bool stable_construction_of_incident_edges) {
+  ASSERT(edge_vector.size() == num_edges);
+
+  EdgeVector edges;
+  edges.reserve(num_edges);
+  for (const auto& e : edge_vector) {
+    if (e.size() != 2) {
+      ERROR("Using graph data structure; but the input hypergraph is not a graph.");
     }
-    return construct_from_graph_edges(num_nodes, num_edges, edges,
-                                      edge_weight, node_weight,
-                                      stable_construction_of_incident_edges);
+    edges.push_back({e[0], e[1]});
   }
+  return construct_from_graph_edges(num_nodes, num_edges, edges,
+                                    edge_weight, node_weight,
+                                    stable_construction_of_incident_edges);
+}
 
-  DynamicGraph DynamicGraphFactory::construct_from_graph_edges(
-          const HypernodeID num_nodes,
-          const HyperedgeID num_edges,
-          const EdgeVector& edge_vector,
-          const HyperedgeWeight* edge_weight,
-          const HypernodeWeight* node_weight,
-          const bool stable_construction_of_incident_edges) {
-    ASSERT(edge_vector.size() == num_edges);
-    DynamicGraph graph;
-    graph._num_edges = 2 * num_edges;
+DynamicGraph DynamicGraphFactory::construct_from_graph_edges(
+        const HypernodeID num_nodes,
+        const HyperedgeID num_edges,
+        const EdgeVector& edge_vector,
+        const HyperedgeWeight* edge_weight,
+        const HypernodeWeight* node_weight,
+        const bool stable_construction_of_incident_edges) {
+  ASSERT(edge_vector.size() == num_edges);
+  DynamicGraph graph;
+  graph._num_edges = 2 * num_edges;
 
-    // TODO: calculate required id range
-    tbb::parallel_invoke([&] {
-      graph._nodes.resize(num_nodes + 1);
-      tbb::parallel_for(ID(0), num_nodes, [&](const HypernodeID n) {
-        // setup nodes
-        DynamicGraph::Hypernode& node = graph._nodes[n];
-        node.enable();
-        if ( node_weight ) {
-          node.setWeight(node_weight[n]);
-        }
-      });
-      // Compute total weight of graph
-      graph.updateTotalWeight(parallel_tag_t());
-    }, [&] {
-      graph._adjacency_array = DynamicAdjacencyArray(num_nodes, edge_vector, edge_weight);
-      if (stable_construction_of_incident_edges) {
-        graph._adjacency_array.sortIncidentEdges();
+  // TODO: calculate required id range
+  tbb::parallel_invoke([&] {
+    graph._nodes.resize(num_nodes + 1);
+    tbb::parallel_for(ID(0), num_nodes, [&](const HypernodeID n) {
+      // setup nodes
+      DynamicGraph::Hypernode& node = graph._nodes[n];
+      node.enable();
+      if ( node_weight ) {
+        node.setWeight(node_weight[n]);
       }
-    }, [&] {
-      graph._acquired_nodes.assign(
-        num_nodes, parallel::IntegralAtomicWrapper<bool>(false));
-    }, [&] {
-      graph._contraction_tree.initialize(num_nodes);
     });
-
-    // Compute total weight of the graph
+    // Compute total weight of graph
     graph.updateTotalWeight(parallel_tag_t());
-    return graph;
-  }
+  }, [&] {
+    graph._adjacency_array = DynamicAdjacencyArray(num_nodes, edge_vector, edge_weight);
+    if (stable_construction_of_incident_edges) {
+      graph._adjacency_array.sortIncidentEdges();
+    }
+  }, [&] {
+    graph._acquired_nodes.assign(
+      num_nodes, parallel::IntegralAtomicWrapper<bool>(false));
+  }, [&] {
+    graph._contraction_tree.initialize(num_nodes);
+  });
+
+  // Compute total weight of the graph
+  graph.updateTotalWeight(parallel_tag_t());
+  return graph;
+}
 
 
 std::pair<DynamicGraph, parallel::scalable_vector<HypernodeID> > DynamicGraphFactory::compactify(const DynamicGraph& graph) {
