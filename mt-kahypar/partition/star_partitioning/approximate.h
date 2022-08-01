78358e29eebd5395ae2417e8c00aead4a88163f9
Nikolai Maas <nikolai.maas@student.kit.edu>
2022-08-01 10:25:27 +0200

    WIP: non-parallel approximation

--- mt-kahypar/partition/star_partitioning/approximate.h
+++ mt-kahypar/partition/star_partitioning/approximate.h
@@ -21,6 +21,7 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
 
 #include "mt-kahypar/datastructures/array.h"
 #include "mt-kahypar/datastructures/streaming_vector.h"
@@ -43,7 +44,7 @@ class Approximate {
   template<typename F, typename G, typename H>
   void partition(const HypernodeID num_nodes, Array<HypernodeWeight>& part_weights,
                  const std::vector<HypernodeWeight>& max_part_weights,
-                 F get_edge_weights_of_node_fn, G get_node_weight_fn, H set_part_id_fn) {
+                 F get_edge_weights_of_node_fn, G get_node_weight_fn, H set_part_id_fn, parallel_tag_t) {
     Array<HyperedgeWeight> gains(num_nodes * _k);
     Array<PartitionID> preferred_part(num_nodes);
 
@@ -93,19 +94,7 @@ class Approximate {
       });
 
       tbb::parallel_sort(local_nodes.begin(), local_nodes.end(),
-        [&](const HypernodeID& left, const HypernodeID& right) {
-          const HypernodeWeight weight_left = get_node_weight_fn(left);
-          const HypernodeWeight weight_right = get_node_weight_fn(right);
-          if (weight_left == 0) {
-            return false;
-          } else if (weight_right == 0) {
-            return true;
-          }
-          return static_cast<double>(get_gain(left)) / weight_left
-                 < static_cast<double>(get_gain(right)) / weight_right;
-        }
-      );
-
+                         compare_gain_weight_ratio(get_gain, get_node_weight_fn));
       std::vector<HypernodeID> excluded = minKnapsack(local_nodes, max_part_weights[part] - part_weights[part],
                                                       get_gain, get_node_weight_fn);
 
@@ -132,9 +121,84 @@ class Approximate {
     );
   }
 
+  template<typename F, typename G, typename H>
+  void partition(const HypernodeID num_nodes, Array<HypernodeWeight>& part_weights,
+                 const std::vector<HypernodeWeight>& max_part_weights,
+                 F get_edge_weights_of_node_fn, G get_node_weight_fn, H set_part_id_fn) {
+    Array<HyperedgeWeight> gains(num_nodes * _k);
+    Array<PartitionID> preferred_part(num_nodes);
+    Array<vec<HypernodeID>> nodes_per_part(_k);
+
+    for (HypernodeID node = 0; node < num_nodes; ++node) {
+      get_edge_weights_of_node_fn(&gains[node * _k], node);
+
+      HyperedgeWeight max_gain = 0;
+      HyperedgeWeight min_gain = std::numeric_limits<HyperedgeWeight>::max();
+      PartitionID max_part = 0;
+      for (PartitionID part = 0; part < _k; ++part) {
+        const HyperedgeWeight gain = gains[node * _k + part];
+        min_gain = std::min(gain, min_gain);
+        if (gain >= max_gain) {
+          max_gain = gain;
+          max_part = part;
+        }
+      }
+      for (PartitionID part = 0; part < _k; ++part) {
+        gains[node * _k + part] -= min_gain;
+      }
+      nodes_per_part[max_part].push_back(node);
+    }
+
+    vec<HypernodeID> unassigned;
+    for (PartitionID part = 0; part < _k; ++part) {
+      auto get_gain = [&](HypernodeID node) { return gains[node * _k + part]; };
+
+      vec<HypernodeID>& nodes = nodes_per_part[part];
+
+      std::sort(nodes.begin(), nodes.end(), compare_gain_weight_ratio(get_gain, get_node_weight_fn));
+      std::vector<HypernodeID> excluded = minKnapsack(nodes, max_part_weights[part] - part_weights[part],
+                                                      get_gain, get_node_weight_fn);
+
+      size_t i = 0;
+      for (const HypernodeID& node: nodes) {
+        if (i < excluded.size() && node == excluded[i]) {
+          unassigned.push_back(node);
+          ++i;
+        } else {
+          set_part_id_fn(node, part);
+          part_weights[part] += get_node_weight_fn(node);
+        }
+      }
+      ALWAYS_ASSERT(i + 1 >= excluded.size());
+    }
+
+    // assign all currently unassigned nodes via the simple greedy algorithm
+    SimpleGreedy sg(_k);
+    sg.partition(unassigned.size(), part_weights, max_part_weights,
+      [&](HyperedgeWeight* weights, const HypernodeID node) { get_edge_weights_of_node_fn(weights, unassigned[node]); },
+      [&](const HypernodeID node) { return get_node_weight_fn(unassigned[node]); },
+      [&](const HypernodeID node, const PartitionID part) { set_part_id_fn(unassigned[node], part); }
+    );
+  }
+
  private:
   PartitionID _k;
 };
+
+template<typename F, typename G>
+auto compare_gain_weight_ratio(F get_gain, G get_node_weight) {
+  return [get_gain, get_node_weight](const HypernodeID& left, const HypernodeID& right) {
+    const HypernodeWeight weight_left = get_node_weight(left);
+    const HypernodeWeight weight_right = get_node_weight(right);
+    if (weight_left == 0) {
+      return false;
+    } else if (weight_right == 0) {
+      return true;
+    }
+    return static_cast<double>(get_gain(left)) / weight_left
+            < static_cast<double>(get_gain(right)) / weight_right;
+  };
+}
 
 
 /*
