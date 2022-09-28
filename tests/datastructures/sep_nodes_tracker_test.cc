49c1f6d5857f14ffad24af6a0223eebdd4baa344
Nikolai Maas <nikolai.maas@student.kit.edu>
2022-09-28 13:54:46 +0200

    randomized test

--- tests/datastructures/sep_nodes_tracker_test.cc
+++ tests/datastructures/sep_nodes_tracker_test.cc
@@ -26,6 +26,7 @@
 #include "mt-kahypar/datastructures/array.h"
 #include "mt-kahypar/datastructures/separated_nodes.h"
 #include "mt-kahypar/datastructures/sep_nodes_tracker.h"
+#include "mt-kahypar/utils/randomize.h"
 
 using ::testing::Test;
 
@@ -109,6 +110,66 @@ class ATracker: public Test {
       }
       tracker.applyMove(s_nodes, part_weights, part_ids, node);
     }
+  }
+
+  HyperedgeWeight calculateCut(const Array<HypernodeWeight>& node_weights,
+                               const std::vector<HypernodeWeight>& max_part_weights,
+                               PartitionID k) const {
+    HyperedgeWeight value = 0;
+    Array<HyperedgeWeight> block_ratings;
+    block_ratings.resize(k, 0);
+    Array<vec<Entry>> block_entries;
+    block_entries.resize(k);
+    Array<HypernodeWeight> block_weights;
+    block_weights.resize(k, 0);
+
+    for (HypernodeID node = 0; node < s_nodes.numNodes(); ++node) {
+      block_ratings.assign(k, 0);
+      HyperedgeWeight total_weight = 0;
+      for (const SeparatedNodes::Edge& e: s_nodes.inwardEdges(node)) {
+        total_weight += e.weight;
+        if (part_ids[e.target] != kInvalidPartition) {
+          block_ratings[part_ids[e.target]] += e.weight;
+        }
+      }
+
+      HyperedgeWeight best = total_weight;
+      HyperedgeWeight second = total_weight;
+      PartitionID best_part = 0;
+      for (PartitionID part = 0; part < k; ++part) {
+        HyperedgeWeight current = total_weight - block_ratings[part];
+        if (current < best) {
+          second = best;
+          best = current;
+          best_part = part;
+        } else if (current < second) {
+          second = current;
+        }
+      }
+      value += best;
+      block_entries[best_part].push_back(Entry{0, s_nodes.nodeWeight(node), second - best});
+    }
+
+    for (size_t i = 0; i < node_weights.size(); ++i) {
+      if (part_ids[i].load() != kInvalidPartition) {
+        block_weights[part_ids[i].load()] += node_weights[i];
+      }
+    }
+    for (PartitionID part = 0; part < k; ++part) {
+      ASSERT(max_part_weights[part] - block_weights[part] == tracker.buckets()[part].allowedWeight(),
+             V(max_part_weights[part]) << V(block_weights[part]) << V(tracker.buckets()[part].allowedWeight()));
+      vec<Entry> entries = block_entries[part];
+      std::sort(entries.rbegin(), entries.rend());
+      const HypernodeWeight allowed_weight = max_part_weights[part] - block_weights[part];
+      HypernodeWeight total_weight = 0;
+      for (const auto& [_, weight, inc_weight]: entries) {
+        total_weight += weight;
+        if (total_weight > allowed_weight) {
+          value += inc_weight;
+        }
+      }
+    }
+    return value;
   }
 
  public:
@@ -340,6 +401,82 @@ TEST_F(ATracker, UnitWeightBasicCases) {
   ASSERT_EQ(3, tracker.rateMove(s_nodes, part_ids, 2, 0, 2, 0, 1));
   setupWithUnitWeights(2, { {{0, 2}}, {{1, 1}}, {{1, 1}} }, {1, 2}, {0, 1});
   ASSERT_EQ(-2, tracker.rateMove(s_nodes, part_ids, 2, 0, 1, 0, 1));
+}
+
+TEST_F(ATracker, RandomizedTestPerfectPredictionForUnitWeightsK2) {
+  const HypernodeWeight max_node_weight = 5;
+  const HyperedgeID max_num_edges = 3;
+  const HyperedgeWeight max_edge_weight = 5;
+  const HypernodeID n_graph_nodes = 8;
+  const HypernodeID n_separated = 10;
+  const PartitionID k = 2;
+
+  utils::Randomize& rand = utils::Randomize::instance();
+  rand.setSeed(0);
+  auto random_part = [&] {
+    PartitionID part = rand.getRandomInt(0, k - 1, sched_getcpu());
+    return part == k ? kInvalidPartition : part;
+  };
+
+  vec<CAtomic<HypernodeWeight>> part_weights;
+  part_weights.resize(k, CAtomic<HypernodeWeight>(0));
+  HypernodeWeight total_weight = n_separated;
+  Array<HypernodeWeight> node_weights;
+  vec<PartitionID> input_part_ids;
+  node_weights.resize(n_graph_nodes);
+  for (HypernodeWeight& weight: node_weights) {
+    weight = rand.getRandomInt(0, max_node_weight, sched_getcpu());
+    total_weight += weight;
+    const PartitionID part = random_part();
+    input_part_ids.push_back(part);
+    if (part != kInvalidPartition) {
+      part_weights[part] += weight;
+    }
+  }
+
+  vec<bool> taken_nodes;
+  vec<vec<std::pair<HypernodeID, HyperedgeWeight>>> separated;
+  for (HypernodeID s_node = 0; s_node < n_separated; ++s_node) {
+    vec<std::pair<HypernodeID, HyperedgeWeight>> edges;
+    const HyperedgeID n_edges = rand.getRandomInt(0, max_num_edges, sched_getcpu());
+    taken_nodes.assign(n_graph_nodes, false);
+    for (HyperedgeID e = 0; e < n_edges; ++e) {
+      HypernodeID target;
+      do {
+        target = rand.getRandomInt(0, n_graph_nodes - 1, sched_getcpu());
+      } while (taken_nodes[target]);
+      taken_nodes[target] = true;
+      edges.emplace_back(target, rand.getRandomInt(0, max_edge_weight, sched_getcpu()));
+    }
+    separated.push_back(std::move(edges));
+  }
+
+  const double capacity = 1.15;
+  const HypernodeWeight max_part_weight = (total_weight * capacity) / k;
+  std::vector<HypernodeWeight> max_part_weights;
+  max_part_weights.resize(k, max_part_weight);
+  setupWithUnitWeights(n_graph_nodes, separated, max_part_weights, input_part_ids, node_weights);
+
+  const HypernodeID num_iterations = 100;
+  for (size_t i = 0; i < num_iterations; ++i) {
+    const HypernodeID node = rand.getRandomInt(0, n_graph_nodes - 1, sched_getcpu());
+    const PartitionID from = part_ids[node].load();
+    PartitionID to = random_part();
+    while (from == to) {
+      to = random_part();
+    }
+    if (part_weights[to] + node_weights[node] > max_part_weights[to]) {
+      continue;
+    }
+    const HyperedgeWeight rating = tracker.rateMove(s_nodes, part_ids, k, node, node_weights[node], from, to);
+    const HyperedgeWeight cut_before = calculateCut(node_weights, max_part_weights, k);
+    part_weights[from] -= node_weights[node];
+    part_weights[to] += node_weights[node];
+    part_ids[node].store(to);
+    tracker.applyMove(s_nodes, part_weights, part_ids, node);
+    const HyperedgeWeight cut_after = calculateCut(node_weights, max_part_weights, k);
+    ASSERT_EQ(cut_before - rating, cut_after) << V(rating) << V(cut_before) << V(cut_after);
+  }
 }
 
 }
