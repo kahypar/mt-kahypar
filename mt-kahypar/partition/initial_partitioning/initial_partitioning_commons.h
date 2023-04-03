2caeffbd9bafaa1acf8b6ad0fae7607ceb511ab4
Tobias Heuer <tobias.heuer@gmx.net>
2023-04-03 11:23:09 +0200

    remove diagnostics

--- mt-kahypar/partition/initial_partitioning/initial_partitioning_commons.h
+++ mt-kahypar/partition/initial_partitioning/initial_partitioning_commons.h
@@ -28,7 +28,6 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#pragma GCC diagnostic ignored "-Wmismatched-tags"
 #include "kahypar/datastructure/kway_priority_queue.h"
 #pragma GCC diagnostic pop
 
