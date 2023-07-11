ef270af2e9a801e6fedb6aa90de16d44cd66ffdb
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2023-07-11 15:26:51 +0200

    recompute penalties before rebalancing in FM

--- mt-kahypar/partition/refinement/fm/multitry_kway_fm.cpp
+++ mt-kahypar/partition/refinement/fm/multitry_kway_fm.cpp
@@ -240,8 +240,16 @@ namespace mt_kahypar {
           tmp_metrics.quality = metrics::quality(phg, context);
           return true;
         }());
+
+        if (!phg.is_graph) {  // TODO add check for whether the rebalancer uses the gain table.
+          tbb::parallel_for(MoveID(0), sharedData.moveTracker.numPerformedMoves(), [&](const MoveID i) {
+            gain_cache.recomputePenaltyTermEntry(phg, sharedData.moveTracker.moveOrder[i].node);
+          });
+        }
+
         tmp_metrics.imbalance = metrics::imbalance(phg, context);
         rebalancer.setMaxPartWeightsForRound(max_part_weights);
+        LOG << "rebalacne from FM";
         rebalancer.refineAndOutputMoves(hypergraph, {}, moves_by_part, tmp_metrics, current_time_limit);
         timer.stop_timer("rebalance_fm");
 
