3c704c8f12282d9556d1b4e513933dabf48b92d9
Nikolai Maas <nikolai.maas@kit.edu>
2023-10-24 16:21:55 +0200

    add stub for spectral refiner

---
+++ mt-kahypar/partition/refinement/spectral/spectral_refiner.cpp
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * This file is part of Mt-KaHyPar.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+#include "mt-kahypar/partition/refinement/spectral/spectral_refiner.h"
+
+#include "mt-kahypar/definitions.h"
+#include "mt-kahypar/partition/metrics.h"
+
+namespace mt_kahypar {
+
+  template <typename GraphAndGainTypes>
+  bool SpectralRefiner<GraphAndGainTypes>::refineImpl(mt_kahypar_partitioned_hypergraph_t& phg,
+                                                      const vec<HypernodeID>& refinement_nodes,
+                                                      Metrics& best_metrics,
+                                                      const double)  {
+    ASSERT(refinement_nodes.empty());  // these are always empty for your case
+    unused(refinement_nodes);
+
+    PartitionedHypergraph& hypergraph = utils::cast<PartitionedHypergraph>(phg);
+    Gain old_quality = best_metrics.quality;
+    // resizeDataStructuresForCurrentK();
+
+    // disable the gain cache for spectral partitioning
+    _gain_cache.reset();
+
+
+    // implementation goes here
+    DBG << "Spectral Refiner called";
+
+
+
+    HEAVY_REFINEMENT_ASSERT(hypergraph.checkTrackedPartitionInformation(_gain_cache));
+    HEAVY_REFINEMENT_ASSERT(best_metrics.quality ==
+      metrics::quality(hypergraph, _context,
+        !_context.refinement.label_propagation.execute_sequential),
+      V(best_metrics.quality) << V(metrics::quality(hypergraph, _context,
+          !_context.refinement.label_propagation.execute_sequential)));
+
+    // Update metrics statistics
+    Gain delta = old_quality - best_metrics.quality;
+    ASSERT(delta >= 0, "Refiner worsened solution quality");
+    utils::Utilities::instance().getStats(_context.utility_id).update_stat("spectral_improvement", delta);
+    return delta > 0;
+  }
+
+
+  template <typename GraphAndGainTypes>
+  void SpectralRefiner<GraphAndGainTypes>::initializeImpl(mt_kahypar_partitioned_hypergraph_t& phg) {
+    unused(phg);
+  }
+
+
+
+  namespace {
+  #define SPECTRAL_REFINER(X) SpectralRefiner<X>
+  }
+
+  // explicitly instantiate so the compiler can generate them when compiling this cpp file
+  INSTANTIATE_CLASS_WITH_VALID_TRAITS(SPECTRAL_REFINER)
+}
---
+++ mt-kahypar/partition/refinement/spectral/spectral_refiner.h
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * This file is part of Mt-KaHyPar.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+#pragma once
+
+#include "mt-kahypar/parallel/stl/scalable_vector.h"
+#include "mt-kahypar/partition/context.h"
+#include "mt-kahypar/partition/refinement/i_refiner.h"
+#include "mt-kahypar/partition/refinement/gains/gain_cache_ptr.h"
+#include "mt-kahypar/utils/cast.h"
+
+
+namespace mt_kahypar {
+template <typename GraphAndGainTypes>
+class SpectralRefiner final : public IRefiner {
+ private:
+  using Hypergraph = typename GraphAndGainTypes::Hypergraph;
+  using PartitionedHypergraph = typename GraphAndGainTypes::PartitionedHypergraph;
+  using GainCache = typename GraphAndGainTypes::GainCache;
+
+  static constexpr bool debug = false;
+  static constexpr bool enable_heavy_assert = false;
+
+ public:
+  explicit SpectralRefiner(const HypernodeID num_hypernodes,
+                           const HyperedgeID num_hyperedges,
+                           const Context& context,
+                           GainCache& gain_cache) :
+    _context(context),
+    _gain_cache(gain_cache) {
+      unused(num_hypernodes);
+      unused(num_hyperedges);
+    }
+
+  explicit SpectralRefiner(const HypernodeID num_hypernodes,
+                                   const HyperedgeID num_hyperedges,
+                                   const Context& context,
+                                   gain_cache_t gain_cache) :
+    SpectralRefiner(num_hypernodes, num_hyperedges, context,
+      GainCachePtr::cast<GainCache>(gain_cache)) { }
+
+  SpectralRefiner(const SpectralRefiner&) = delete;
+  SpectralRefiner(SpectralRefiner&&) = delete;
+
+  SpectralRefiner & operator= (const SpectralRefiner &) = delete;
+  SpectralRefiner & operator= (SpectralRefiner &&) = delete;
+
+ private:
+  bool refineImpl(mt_kahypar_partitioned_hypergraph_t& hypergraph,
+                  const parallel::scalable_vector<HypernodeID>& refinement_nodes,
+                  Metrics& best_metrics,
+                  double) final;
+
+  void initializeImpl(mt_kahypar_partitioned_hypergraph_t&) final;
+
+  // void resizeDataStructuresForCurrentK() {
+  //
+  // }
+
+  const Context& _context;
+  GainCache& _gain_cache;
+};
+
+}  // namespace kahypar
