b68cfc022c0fbd8e7a15e72062737d5bab5dcf25
Julian <unkjx@student.kit.edu>
2023-11-24 17:57:47 +0100

    add slepc ex3 to solver class

--- mt-kahypar/partition/refinement/spectral/solvers/slepc_gevp.cpp
+++ mt-kahypar/partition/refinement/spectral/solvers/slepc_gevp.cpp
@@ -25,18 +25,150 @@
 #include "mt-kahypar/partition/refinement/spectral/solvers/gevp.h"
 
 #include <slepceps.h>
-#include <slepcsys.h>
 
 namespace mt_kahypar {
 namespace spectral {
 
+char help[] = "Solves the same eigenproblem as in example ex2, but using a shell matrix. "
+  "The problem is a standard symmetric eigenproblem corresponding to the 2-D Laplacian operator.\n\n"
+  "The command line options are:\n"
+  "  -n <n>, where <n> = number of grid subdivisions in both x and y dimensions.\n\n";
+
+static void tv(int nx,const PetscScalar *x,PetscScalar *y){
+  PetscScalar dd,dl,du;
+  int         j;
+
+  dd  = 4.0;
+  dl  = -1.0;
+  du  = -1.0;
+
+  y[0] =  dd*x[0] + du*x[1];
+  for (j=1;j<nx-1;j++)
+    y[j] = dl*x[j-1] + dd*x[j] + du*x[j+1];
+  y[nx-1] = dl*x[nx-2] + dd*x[nx-1];
+}
+
+PetscErrorCode MatMult_Laplacian2D(Mat A,Vec x,Vec y) {
+  void              *ctx;
+  int               nx,lo,i,j;
+  const PetscScalar *px;
+  PetscScalar       *py;
+
+  PetscFunctionBeginUser;
+  PetscCall(MatShellGetContext(A,&ctx));
+  nx = *(int*)ctx;
+  PetscCall(VecGetArrayRead(x,&px));
+  PetscCall(VecGetArray(y,&py));
+
+  tv(nx,&px[0],&py[0]);
+  for (i=0;i<nx;i++) py[i] -= px[nx+i];
+
+  for (j=2;j<nx;j++) {
+    lo = (j-1)*nx;
+    tv(nx,&px[lo],&py[lo]);
+    for (i=0;i<nx;i++) py[lo+i] -= px[lo-nx+i] + px[lo+nx+i];
+  }
+
+  lo = (nx-1)*nx;
+  tv(nx,&px[lo],&py[lo]);
+  for (i=0;i<nx;i++) py[lo+i] -= px[lo-nx+i];
+
+  PetscCall(VecRestoreArrayRead(x,&px));
+  PetscCall(VecRestoreArray(y,&py));
+  PetscFunctionReturn(PETSC_SUCCESS);
+};
+  
+PetscErrorCode MatGetDiagonal_Laplacian2D(Mat A,Vec diag) {
+  PetscFunctionBeginUser;
+  PetscCall(VecSet(diag,4.0));
+  PetscFunctionReturn(PETSC_SUCCESS);
+};
+
+int ex3_main(int argc, char **argv){
+  Mat            A;               /* operator matrix */
+  EPS            eps;             /* eigenproblem solver context */
+  EPSType        type;
+  PetscMPIInt    size;
+  PetscInt       N,n=10,nev;
+  PetscBool      terse;
+
+  PetscFunctionBeginUser;
+  PetscCall(SlepcInitialize(&argc,&argv,(char*)0,help));
+  PetscCallMPI(MPI_Comm_size(PETSC_COMM_WORLD,&size));
+  PetscCheck(size==1,PETSC_COMM_WORLD,PETSC_ERR_WRONG_MPI_SIZE,"This is a uniprocessor example only");
+
+  PetscCall(PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL));
+  N = n*n;
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD,"\n2-D Laplacian Eigenproblem (matrix-free version), N=%" PetscInt_FMT " (%" PetscInt_FMT "x%" PetscInt_FMT " grid)\n\n",N,n,n));
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      Create the operator matrix that defines the eigensystem, Ax=kx
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  PetscCall(MatCreateShell(PETSC_COMM_WORLD,N,N,N,N,&n,&A));
+  PetscCall(MatShellSetOperation(A,MATOP_MULT,(void(*)(void))MatMult_Laplacian2D));
+  PetscCall(MatShellSetOperation(A,MATOP_MULT_TRANSPOSE,(void(*)(void))MatMult_Laplacian2D));
+  PetscCall(MatShellSetOperation(A,MATOP_GET_DIAGONAL,(void(*)(void))MatGetDiagonal_Laplacian2D));
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                Create the eigensolver and set various options
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /*
+    Create eigensolver context
+  */
+  PetscCall(EPSCreate(PETSC_COMM_WORLD,&eps));
+
+  /*
+    Set operators. In this case, it is a standard eigenvalue problem
+  */
+  PetscCall(EPSSetOperators(eps,A,NULL));
+  PetscCall(EPSSetProblemType(eps,EPS_HEP));
+
+  /*
+    Set solver parameters at runtime
+  */
+  PetscCall(EPSSetFromOptions(eps));
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                      Solve the eigensystem
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  PetscCall(EPSSolve(eps));
+
+  /*
+    Optional: Get some information from the solver and display it
+  */
+  PetscCall(EPSGetType(eps,&type));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD," Solution method: %s\n\n",type));
+  PetscCall(EPSGetDimensions(eps,&nev,NULL,NULL));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD," Number of requested eigenvalues: %" PetscInt_FMT "\n",nev));
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                    Display solution and clean up
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* show detailed info unless -terse option is given by user */
+  PetscCall(PetscOptionsHasName(NULL,NULL,"-terse",&terse));
+  if (terse) PetscCall(EPSErrorView(eps,EPS_ERROR_RELATIVE,NULL));
+  else {
+    PetscCall(PetscViewerPushFormat(PETSC_VIEWER_STDOUT_WORLD,PETSC_VIEWER_ASCII_INFO_DETAIL));
+    PetscCall(EPSConvergedReasonView(eps,PETSC_VIEWER_STDOUT_WORLD));
+    PetscCall(EPSErrorView(eps,EPS_ERROR_RELATIVE,PETSC_VIEWER_STDOUT_WORLD));
+    PetscCall(PetscViewerPopFormat(PETSC_VIEWER_STDOUT_WORLD));
+  }
+  PetscCall(EPSDestroy(&eps));
+  PetscCall(MatDestroy(&A));
+  PetscCall(SlepcFinalize());
+  return 0;
+}
+
 class SLEPcGEVPSolver : public spectral::GEVPSolver {
  public:
-  void initialize(spectral::Matrix& a, spectral::Matrix& b) final {}
-  bool nextEigenpair(spectral::Skalar& eval, spectral::Vector& evec) final {
-    int ierr = SlepcInitialize(NULL, NULL, (char*)0, "");
-    return true;
-  }
+  void initialize(spectral::Matrix& a, spectral::Matrix& b) final {
+    ex3_main(0, NULL);
+  };
+  bool nextEigenpair(spectral::Skalar& eval, spectral::Vector& evec) final {}
 };
 
 }
