a1e489c5382d0fbf58e41065733798db20fe3e6a
Julian <unkjx@student.kit.edu>
2024-06-12 15:14:16 +0200

    add all changes for overlaying

--- mt-kahypar/partition/refinement/spectral/solvers/julia/overlay.jl
+++ mt-kahypar/partition/refinement/spectral/solvers/julia/overlay.jl
@@ -1,19 +1,20 @@
 include("overlay/contraction.jl")
 
-function overlay_partitions(partitions, hgr)
-    hgraph_contracted, clusters = overlay(best_partitions, hgraph)
-    refined_partition = optimal_partitioner(hmetis_path, ilp_path, hgraph_contracted, num_parts, ub_factor)
-    cutsize = golden_evaluator(hgraph_contracted, num_parts, refined_partition)
-    partition_projected = zeros(Int, hgraph.num_vertices)
+function overlay_partitions(partitions, hgr, hgr_file)
+    ub_factor = ceil(Int, config_e * 100)
+    hgraph_contracted, clusters = overlay(partitions, hgr)
+    refined_partition = optimal_partitioner(hgraph_contracted, config_k, ub_factor)
+    cutsize = golden_evaluator(hgraph_contracted, config_k, refined_partition)
+    partition_projected = zeros(Int, hgr.num_vertices)
     for i in 1:length(clusters)
         cid = clusters[i]
         partition_projected[i] = refined_partition[cid]
     end
-    specpart_partition_name = processed_hg_name * ".specpart" * ".part.2"
+    specpart_partition_name = hgr_file * ".specpart" * ".part.2"
     write_partition(partition_projected, specpart_partition_name)
-    triton_part_refine(triton_part_refiner_path, processed_hg_name, specpart_partition_name, num_parts, ub_factor, seed, 0)
+    triton_part_refine(hgr_file, specpart_partition_name, config_k, ub_factor, config_seed, 0)
     partition_projected = read_hint_file(specpart_partition_name)
-    cutsize = golden_evaluator(hgraph, num_parts, partition_projected)
+    cutsize = golden_evaluator(hgr, config_k, partition_projected)
     
     return partition_projected, cutsize
 end
