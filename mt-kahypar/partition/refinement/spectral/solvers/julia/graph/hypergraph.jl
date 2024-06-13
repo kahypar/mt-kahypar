d07c355378ebceec6c6f3c574db322a4666d1f88
Julian <unkjx@student.kit.edu>
2024-06-13 21:17:47 +0200

    format

--- mt-kahypar/partition/refinement/spectral/solvers/julia/graph/hypergraph.jl
+++ mt-kahypar/partition/refinement/spectral/solvers/julia/graph/hypergraph.jl
@@ -126,32 +126,32 @@ end
 
 function write_partition(partition::Vector{Int},
     partition_file_name::String)
-f = open(partition_file_name, "w")
-for i in 1:length(partition)
-println(f, partition[i])
-end
-close(f)
+    f = open(partition_file_name, "w")
+    for i in 1:length(partition)
+        println(f, partition[i])
+    end
+    close(f)
 end
 
 function golden_evaluator(hypergraph::__hypergraph__,
     num_parts::Int,
     partition::Vector{Int})
-cutsize = 0
-for i in 1:hypergraph.num_hyperedges
-first_valid_entry = hypergraph.eptr[i]
-first_invalid_entry = hypergraph.eptr[i+1]
-for j in first_valid_entry+1 : first_invalid_entry-1
-v = hypergraph.eind[j]
-if (partition[hypergraph.eind[j]] != partition[hypergraph.eind[j-1]])
-cutsize += hypergraph.hwts[i]
-break
-end
-end
-end
-balance = zeros(Int, num_parts)
-for i in 1:hypergraph.num_vertices
-p = partition[i]
-balance[p+1] += hypergraph.vwts[i]
-end
-return (cutsize, balance)
+    cutsize = 0
+    for i in 1:hypergraph.num_hyperedges
+        first_valid_entry = hypergraph.eptr[i]
+        first_invalid_entry = hypergraph.eptr[i+1]
+        for j in first_valid_entry+1 : first_invalid_entry-1
+            v = hypergraph.eind[j]
+            if (partition[hypergraph.eind[j]] != partition[hypergraph.eind[j-1]])
+                cutsize += hypergraph.hwts[i]
+                break
+            end
+        end
+    end
+    balance = zeros(Int, num_parts)
+    for i in 1:hypergraph.num_vertices
+        p = partition[i]
+        balance[p+1] += hypergraph.vwts[i]
+    end
+    return (cutsize, balance)
 end
