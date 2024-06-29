ea754d001194950131c362649570cf478ed1a8cc
konpklr <konstantinvpueckler2408@gmail.com>
2024-06-29 13:58:12 -0400

    fixed full level hierarchy readin

--- mt-kahypar/partition/coarsening/multilevel_coarsener.h
+++ mt-kahypar/partition/coarsening/multilevel_coarsener.h
@@ -229,6 +229,7 @@ class MultilevelCoarsener : public ICoarsener,
     for(HypernodeID hn : current_hg.nodes()){
       //if(current_hg.nodeWeight(hn).weights[1] == 0) std::cout << 1/0;
     }
+    std::cout << "c0: " << cluster_ids[0] << current_hg.nodeWeight(0).weights[0] << " " << current_hg.nodeWeight(0).weights[1] << "\n";
     for(int i = 0; i < cluster_ids.size(); i++){
       if(cluster_ids[i] != ctmp[i]) std::cout << 1/0 << "\n";
       tmp[cluster_ids[i]] += current_hg.nodeWeight(i);
