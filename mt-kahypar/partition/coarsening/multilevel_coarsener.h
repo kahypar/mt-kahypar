cfee7fc254993de2f89e602b82bdb0032e40a6cd
konpklr <konstantinvpueckler2408@gmail.com>
2024-06-29 12:15:43 -0400

    fixed full level hierarchy readin

--- mt-kahypar/partition/coarsening/multilevel_coarsener.h
+++ mt-kahypar/partition/coarsening/multilevel_coarsener.h
@@ -223,6 +223,7 @@ class MultilevelCoarsener : public ICoarsener,
     // Perform parallel contraction
     _uncoarseningData.performMultilevelContraction(std::move(cluster_ids), true /* deterministic */, round_start);
     Hypergraph& chg = Base::currentHypergraph();
+    if(chg.initialNumNodes() == current_hg.initialNumNodes()) std::cout << 1/0 << "\n";
     std::vector<HypernodeWeight> tmp(chg.initialNumNodes(), 0);
     for(int i = 0; i < cluster_ids.size(); i++){
       if(cluster_ids[i] != ctmp[i]) std::cout << 1/0 << "\n";
