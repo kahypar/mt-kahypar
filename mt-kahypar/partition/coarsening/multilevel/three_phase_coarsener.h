015c8a6cc0253f705f8a4abf47214844e53dd528
Nikolai Maas <nikolai.maas@kit.edu>
2024-09-06 15:13:40 +0200

    timers

--- mt-kahypar/partition/coarsening/multilevel/three_phase_coarsener.h
+++ mt-kahypar/partition/coarsening/multilevel/three_phase_coarsener.h
@@ -146,9 +146,9 @@ class ThreePhaseCoarsener : public ICoarsener,
                                      _rater, _clustering_data, _num_nodes_tracker);
     cc.initializeCoarseningPass(current_hg, _context);
     _timer.start_timer("init_similarity", "Initialize Similarity Data");
-    _similarity_policy.initialize(current_hg, _context);
+    _similarity_policy.initialize(current_hg, _context, _timer);
     _timer.stop_timer("init_similarity");
-    _always_accept_policy.initialize(current_hg, _context);
+    _always_accept_policy.initialize(current_hg, _context, _timer);
 
     if (shouldIgnoreCommunities(hierarchy_contraction_limit)) {
       cc.may_ignore_communities = true;
