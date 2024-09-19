c306faa71e5e173d0644055c741976414da979e8
Nikolai Maas <nikolai.maas@kit.edu>
2024-09-19 17:21:12 +0200

    python summation script

---
+++ frequencies.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+import polars as pl
+import altair as alt
+
+###########################
+
+def build_histogram(data: pl.DataFrame, filename: str, max: int):
+    histogram = data["frequency"].hist(bins=[x for x in range(0, max + 1)], include_category=False)
+    histogram = histogram.select("breakpoint", "count").filter(histogram["breakpoint"] <= max)
+    print(histogram)
+    plot = alt.Chart(histogram).mark_bar().encode(
+        x=alt.X("breakpoint"),
+        y=alt.Y("count").scale(type="symlog"),
+    )
+    plot.save(filename)
+
+
+files = sys.argv[1:]
+
+schema = {"id_high_degree": pl.Int64, "id_low_degree": pl.Int64, "k": pl.Int64, "frequency": pl.Int64}
+accumulated = pl.DataFrame({}, schema=schema)
+for f in files:
+    data = pl.read_csv(f, comment_prefix="#", schema={"id_high_degree": pl.Int64, "id_low_degree": pl.Int64, "frequency": pl.Int64})
+    print(data)
+    k = None
+    for i in range(2, 128):
+        if f"k{i}" in f:
+            k = i
+    data_with_k = data.with_columns(k=pl.repeat(k, data.select(pl.len()).item(), dtype=pl.Int64))
+    accumulated = pl.concat([accumulated, data_with_k], how="diagonal")
+    build_histogram(data, f + ".histogram.pdf", 10)
+
+summed = accumulated.group_by("id_high_degree", "id_low_degree").agg(pl.col("frequency").sum())
+print(summed)
+build_histogram(summed, "summed.histogram.pdf", 10 * len(files))
