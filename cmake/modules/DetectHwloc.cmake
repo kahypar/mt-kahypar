2c85d4f83980fe0bc6e0c7f6c910322d2518c462
Nikolai Maas <nikolai.maas@kit.edu>
2024-11-18 15:55:07 +0100

    support static linking as far as possible

--- cmake/modules/DetectHwloc.cmake
+++ cmake/modules/DetectHwloc.cmake
@@ -2,6 +2,12 @@
 set(HWLOC_TARGET_NAME HWLOC::hwloc)
 
 if(NOT TARGET ${HWLOC_TARGET_NAME})
+    if(NOT BUILD_SHARED_LIBS AND KAHYPAR_STATIC_LINK_DEPENDENCIES)
+        set(CMAKE_FIND_LIBRARY_SUFFIXES .a .lib)
+    endif()
+
+    add_library(hwloc INTERFACE)
+
     # search predefined paths
     find_path(HWLOCK_INCLUDE_DIR NAME hwloc.h)
     find_library(HWLOCK_LIBRARY NAME hwloc)
@@ -10,15 +16,22 @@ if(NOT TARGET ${HWLOC_TARGET_NAME})
         add_library(hwloc INTERFACE IMPORTED)
         target_include_directories(hwloc INTERFACE ${HWLOCK_INCLUDE_DIR})
         target_link_libraries(hwloc INTERFACE ${HWLOCK_LIBRARY})
-        add_library(${HWLOC_TARGET_NAME} ALIAS hwloc)
     elseif(UNIX)
         # search for hwloc via pkg_config
         find_package(PkgConfig QUIET)
         if (PKG_CONFIG_FOUND)
             pkg_search_module(HWLOC hwloc IMPORTED_TARGET)
             if (TARGET PkgConfig::HWLOC)
-                add_library(${HWLOC_TARGET_NAME} ALIAS PkgConfig::HWLOC)
+                target_link_libraries(hwloc INTERFACE PkgConfig::HWLOC)
             endif()
         endif()
     endif()
+
+    if(NOT BUILD_SHARED_LIBS AND KAHYPAR_STATIC_LINK_DEPENDENCIES)
+        # for static linking we also need udev as transitive dependency of hwloc
+        target_link_options(hwloc INTERFACE -ludev)
+    endif()
+
+    # actual target for outside use
+    add_library(${HWLOC_TARGET_NAME} ALIAS hwloc)
 endif()
