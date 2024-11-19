4a61340abaf0d17fe47a462886e8c5d5ae5c21f9
Nikolai Maas <nikolai.maas@kit.edu>
2024-11-19 14:49:54 +0100

    final (?) fix for hwloc

--- cmake/modules/DetectHwloc.cmake
+++ cmake/modules/DetectHwloc.cmake
@@ -1,33 +1,51 @@
-# custom script for finding the hwloc library, creates a HWLOC::hwloc target
+# Custom script for finding the hwloc library, creates a HWLOC::hwloc target.
+# Some complications are necessary in order to also support static linking.
 set(HWLOC_TARGET_NAME HWLOC::hwloc)
+if(NOT BUILD_SHARED_LIBS AND KAHYPAR_STATIC_LINK_DEPENDENCIES)
+    set(HWLOC_LINK_STATICALLY TRUE)
+endif()
+
+function(find_hwloc_path)
+    # search predefined paths
+    find_path(HWLOC_INCLUDE_DIR NAMES hwloc.h)
+    find_library(HWLOC_LIBRARY NAMES hwloc)
+    if(HWLOC_INCLUDE_DIR AND HWLOC_LIBRARY)
+        message(STATUS "Found hwloc library: inc=${HWLOC_INCLUDE_DIR}, lib=${HWLOC_LIBRARY}")
+        target_include_directories(hwloc INTERFACE ${HWLOC_INCLUDE_DIR})
+        target_link_libraries(hwloc INTERFACE ${HWLOC_LIBRARY})
+        set(HWLOC_FOUND TRUE PARENT_SCOPE)
+    endif()
+endfunction()
 
 if(NOT TARGET ${HWLOC_TARGET_NAME})
-    if(NOT BUILD_SHARED_LIBS AND KAHYPAR_STATIC_LINK_DEPENDENCIES)
+    if(HWLOC_LINK_STATICALLY)
         set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
     endif()
+    add_library(hwloc INTERFACE IMPORTED)
 
-    add_library(hwloc INTERFACE)
-
-    # search predefined paths
-    find_path(HWLOCK_INCLUDE_DIR NAME hwloc.h)
-    find_library(HWLOCK_LIBRARY NAME hwloc)
-    if(HWLOCK_INCLUDE_DIR AND HWLOCK_LIBRARY)
-        message(STATUS "Found hwlock library: inc=${HWLOCK_INCLUDE_DIR}, lib=${HWLOCK_LIBRARY}")
-        add_library(hwloc INTERFACE IMPORTED)
-        target_include_directories(hwloc INTERFACE ${HWLOCK_INCLUDE_DIR})
-        target_link_libraries(hwloc INTERFACE ${HWLOCK_LIBRARY})
-    elseif(UNIX)
+    find_hwloc_path()
+    if(NOT HWLOC_FOUND AND HWLOC_LINK_STATICALLY)
+        message(WARNING "Could not find static version of hwloc library. Proceeding to link dynamically.")
+        set(HWLOC_LINK_STATICALLY FALSE)
+        unset(CMAKE_FIND_LIBRARY_SUFFIXES)
+        find_hwloc_path()
+    endif()
+    if(NOT HWLOC_FOUND)
         # search for hwloc via pkg_config
         find_package(PkgConfig QUIET)
         if (PKG_CONFIG_FOUND)
             pkg_search_module(HWLOC hwloc IMPORTED_TARGET)
             if (TARGET PkgConfig::HWLOC)
                 target_link_libraries(hwloc INTERFACE PkgConfig::HWLOC)
+                set(HWLOC_FOUND TRUE)
             endif()
         endif()
     endif()
+    if(NOT HWLOC_FOUND)
+        message(FATAL_ERROR "hwloc library not found. Install hwloc on your system.")
+    endif()
 
-    if(NOT BUILD_SHARED_LIBS AND KAHYPAR_STATIC_LINK_DEPENDENCIES)
+    if(HWLOC_LINK_STATICALLY)
         # for static linking we also need udev as transitive dependency of hwloc
         find_package(PkgConfig QUIET REQUIRED)
         if (PKG_CONFIG_FOUND)
