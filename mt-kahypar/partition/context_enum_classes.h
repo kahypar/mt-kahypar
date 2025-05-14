1d30a8780dad561df1d92fe85fb142e3e80ba69d
Nikolai Maas <nikolai.maas@kit.edu>
2025-05-14 14:50:45 +0200

    Jet: initial implementation

--- mt-kahypar/partition/context_enum_classes.h
+++ mt-kahypar/partition/context_enum_classes.h
@@ -154,6 +154,11 @@ enum class LabelPropagationAlgorithm : uint8_t {
   do_nothing
 };
 
+enum class JetAlgorithm : uint8_t {
+  deterministic,
+  do_nothing
+};
+
 enum class FMAlgorithm : uint8_t {
   kway_fm,
   unconstrained_fm,
@@ -217,6 +222,8 @@ std::ostream & operator<< (std::ostream& os, const InitialPartitioningAlgorithm&
 
 std::ostream & operator<< (std::ostream& os, const LabelPropagationAlgorithm& algo);
 
+std::ostream & operator<< (std::ostream& os, const JetAlgorithm& algo);
+
 std::ostream & operator<< (std::ostream& os, const FMAlgorithm& algo);
 
 std::ostream & operator<< (std::ostream& os, const FlowAlgorithm& algo);
@@ -250,6 +257,8 @@ RatingFunction ratingFunctionFromString(const std::string& function);
 InitialPartitioningAlgorithm initialPartitioningAlgorithmFromString(const std::string& algo);
 
 LabelPropagationAlgorithm labelPropagationAlgorithmFromString(const std::string& type);
+
+JetAlgorithm jetAlgorithmFromString(const std::string& type);
 
 FMAlgorithm fmAlgorithmFromString(const std::string& type);
 
