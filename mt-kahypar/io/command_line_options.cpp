ea98888c7a221d3a648e8555528c747c96b602bb
Nikolai Maas <nikolai.maas@kit.edu>
2025-07-25 10:38:57 +0200

    implement two-hop coarsening

--- mt-kahypar/io/command_line_options.cpp
+++ mt-kahypar/io/command_line_options.cpp
@@ -314,6 +314,18 @@ namespace mt_kahypar {
             ("c-resolve-swaps",
              po::value<bool>(&context.coarsening.det_resolve_swaps)->value_name("<bool>")->default_value(true),
              "Whether to resolve node swaps in a postprocessing step for deterministic coarsening.")
+            ("c-two-hop-required-similarity",
+             po::value<double>(&context.coarsening.twin_required_similarity)->value_name(
+                     "<double>")->default_value(0.75),
+             "Required neighborhood similarity for two-hop coarsening.")
+            ("c-twin-min-relative-connectivity",
+             po::value<double>(&context.coarsening.twin_min_relative_connectivity)->value_name(
+                     "<double>")->default_value(0.1),
+             "Required relative connectivity to a cluster to include this neighbor in the twin matching neighborhood.")
+            ("c-degree-one-node-cluster-size",
+             po::value<HypernodeID>(&context.coarsening.degree_one_node_cluster_size)->value_name(
+                     "<int>")->default_value(4),
+             "Two-hop coarsening: maximum number of degree one nodes in one cluster.")
             ("c-sim-incident-weight-scaling",
              po::value<uint32_t>(&context.coarsening.rating.incident_weight_scaling_constant)->value_name(
                      "<int>")->default_value(1),
