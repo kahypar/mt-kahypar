1d98ca1063c90fd98b8e4ee3f75577f670df21dd
Molpich <jason.mark.hayes@gmail.com>
2025-10-12 23:19:41 +0200

    Remove unnecessary file

--- benchmarks/test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Check environment variables
-# Check current directory
-
-# use %USERPROFILE%
-os.system("C:/Users/Kantfan/Bachelor_Arbeit/mt-kahypar/mt-kahypar/benchmarks/scripts/mt_kahypar.py C:/Users/Kantfan/Bachelor_Arbeit/mt-kahypar/mt-kahypar/benchmarks/instances/test_instance.hgr 8 8 0.03 2 km1 5 --name 'Mt-KaHyPar-evo' --args ' kaka' ")                             # Unix/Linux/MinGW
