15aa734e9d952e85209b9a872203bb0eab15e759
Nikolai Maas <nikolai.maas@kit.edu>
2025-12-08 13:01:42 +0100

    fix scalability bottleneck for two-hop clustering (#228)

--- mt-kahypar/partition/coarsening/multilevel/two_hop_clustering.cpp
+++ mt-kahypar/partition/coarsening/multilevel/two_hop_clustering.cpp
@@ -26,6 +26,8 @@
 
 #include "mt-kahypar/partition/coarsening/multilevel/two_hop_clustering.h"
 
+#include <thread>
+
 #include <tbb/parallel_for.h>
 
 #include "mt-kahypar/definitions.h"
@@ -35,10 +37,13 @@ namespace mt_kahypar {
 
 TwoHopClustering::TwoHopClustering(const HypernodeID num_nodes, const Context& context):
   _context(context),
+  _cluster_count(num_nodes, AtomicID(0)),
+  _local_cluster_count(std::thread::hardware_concurrency()),
   _favorite_clusters(),
   _local_incidence_map([=] {
     return CacheEfficienIncidenceMap(3UL * std::min(UL(num_nodes), _context.coarsening.two_hop_degree_threshold), 0.0);
-  }) {
+  }),
+  _local_collected_nodes(std::thread::hardware_concurrency()) {
     if (_context.coarsening.two_hop_cluster_size < 2) {
       throw InvalidParameterException("Value for c-two-hop-cluster-size too small, must be at least 2");
     }
@@ -49,8 +54,23 @@ void TwoHopClustering::performClustering(const Hypergraph& hg,
                                          const vec<HypernodeID>& node_mapping,
                                          ClusteringContext<Hypergraph>& cc,
                                          bool has_fixed_vertices) {
-  _favorite_clusters.clearParallel();
-  _favorite_clusters.reserve_for_estimated_number_of_insertions(cc.currentNumNodes() / 3);
+  // reset
+  tbb::parallel_invoke([&] {
+    tbb::parallel_for(ID(0), hg.initialNumNodes(), [&](HypernodeID hn) {
+      ASSERT(hn < _cluster_count.size());
+      _cluster_count[hn] = 0;
+    });
+  }, [&] {
+    for (auto& map: _local_cluster_count) {
+      map.clear();
+    }
+    for (auto& bucket: _local_collected_nodes) {
+      bucket.clear();
+    }
+  }, [&] {
+    _favorite_clusters.clearParallel();
+    _favorite_clusters.reserve_for_estimated_number_of_insertions(cc.currentNumNodes() / 3);
+  });
 
   auto fill_incidence_map_for_node = [&](auto& incidence_map, const HypernodeID hn, bool& too_many_accesses) {
     if constexpr (Hypergraph::is_graph) {
@@ -80,7 +100,7 @@ void TwoHopClustering::performClustering(const Hypergraph& hg,
     }
   };
 
-  // insert unmatched (low degree) nodes into buckets based on their favorite cluster
+  // collect unmatched (low degree) nodes and compute their favorite cluster
   tbb::parallel_for(ID(0), hg.initialNumNodes(), [&](const HypernodeID id) {
     ASSERT(id < node_mapping.size());
     const HypernodeID hn = node_mapping[id];
@@ -103,42 +123,78 @@ void TwoHopClustering::performClustering(const Hypergraph& hg,
           }
         }
         if (best_target != kInvalidHypernode) {
-          _favorite_clusters.insert(best_target, MatchingEntry{best_target, hn});
+          const size_t thread_id = THREAD_ID;
+          _local_collected_nodes[thread_id].push_back(MatchingEntry{best_target, hn});
+          _local_cluster_count[thread_id][best_target]++;
         }
       }
     }
   });
 
-  auto bucket_comparator = [&](const MatchingEntry& lhs, const MatchingEntry& rhs) {
-    if (lhs.key == rhs.key) {
-      return hg.communityID(lhs.hn) < hg.communityID(rhs.hn);
-    } else {
-      return lhs.key < rhs.key;
+  // compute the total count of incident nodes for each cluster
+  tbb::parallel_for(UL(0), _local_cluster_count.size(), [&](size_t thread_id) {
+    const LocalCountingMap& local_count = _local_cluster_count[thread_id];
+    for (const auto& entry: local_count) {
+      _cluster_count[entry.key].fetch_add(entry.value, std::memory_order_relaxed);
     }
-  };
+  });
 
-  // match nodes that have the same favorite cluster
+  tbb::parallel_for(UL(0), _local_collected_nodes.size(), [&](size_t thread_id) {
+    vec<MatchingEntry>& local_nodes = _local_collected_nodes[thread_id];
+
+    // insert nodes incident to "low-degree" clusters into global bucket map, so that
+    // all possible contraction partners can be found
+    local_nodes.erase(
+      std::remove_if(local_nodes.begin(), local_nodes.end(), [&](const MatchingEntry& entry) {
+        if (_cluster_count[entry.target].load(std::memory_order_relaxed) < LARGE_CLUSTER_DEGREE_THRESHOLD) {
+          _favorite_clusters.insert(entry.target, MatchingEntry(entry));
+          return true;
+        }
+        return false;
+      }),
+      local_nodes.end());
+
+    // cluster nodes incident to "high-degree" clusters locally, to avoid scalability bottlenecks
+    matchVerticesInBucket(hg, cc, local_nodes, has_fixed_vertices);
+  });
+
   tbb::parallel_for(UL(0), _favorite_clusters.numBuckets(), [&](const size_t bucket_id) {
     auto& bucket = _favorite_clusters.getBucket(bucket_id);
-    std::sort(bucket.begin(), bucket.end(), bucket_comparator);
-
-    const HypernodeID max_size = _context.coarsening.two_hop_cluster_size;
-    for (size_t i = 0; i + 1 < bucket.size() && cc.shouldContinue(); ++i) {
-      HypernodeID num_matches = 0;
-      for (size_t j = i + 1; j < i + max_size && j < bucket.size() && cc.shouldContinue()
-            && bucket[i].key == bucket[j].key && hg.communityID(bucket[i].hn) == hg.communityID(bucket[j].hn); ++j) {
-        ASSERT((j > i + 1 || cc.vertexIsUnmatched(bucket[i].hn)) && cc.vertexIsUnmatched(bucket[j].hn));
-        // Note: cluster weight and fixed vertices are checked by `matchVertices` (might not succeed)
-        // j must be left, since only the right node is allowed to already be matched
-        bool success = cc.matchVertices(hg, bucket[j].hn, bucket[i].hn, has_fixed_vertices);
-        if (!success) {
-          break;
-        }
-        num_matches++;
-      }
-      i += num_matches;
-    }
+    matchVerticesInBucket(hg, cc, bucket, has_fixed_vertices);
   });
+}
+
+template<typename Hypergraph>
+void TwoHopClustering::matchVerticesInBucket(const Hypergraph& hg,
+                                             ClusteringContext<Hypergraph>& cc,
+                                             vec<MatchingEntry>& bucket,
+                                             bool has_fixed_vertices) {
+  auto bucket_comparator = [&](const MatchingEntry& lhs, const MatchingEntry& rhs) {
+    if (lhs.target == rhs.target) {
+      return hg.communityID(lhs.hn) < hg.communityID(rhs.hn);
+    } else {
+      return lhs.target < rhs.target;
+    }
+  };
+  std::sort(bucket.begin(), bucket.end(), bucket_comparator);
+
+  // match nodes that have the same favorite cluster
+  const HypernodeID max_size = _context.coarsening.two_hop_cluster_size;
+  for (size_t i = 0; i + 1 < bucket.size() && cc.shouldContinue(); ++i) {
+    HypernodeID offset = 0;
+    for (size_t j = i + 1; j < i + max_size && j < bucket.size() && cc.shouldContinue()
+          && bucket[i].target == bucket[j].target && hg.communityID(bucket[i].hn) == hg.communityID(bucket[j].hn); ++j) {
+      ASSERT((j > i + 1 || cc.vertexIsUnmatched(bucket[i].hn)) && cc.vertexIsUnmatched(bucket[j].hn));
+      // Note: cluster weight and fixed vertices are checked by `matchVertices` (might not succeed)
+      // j must be left, since only the right node is allowed to already be matched
+      bool success = cc.matchVertices(hg, bucket[j].hn, bucket[i].hn, has_fixed_vertices);
+      if (!success) {
+        break;
+      }
+      offset++;
+    }
+    i += offset;
+  }
 }
 
 namespace {
