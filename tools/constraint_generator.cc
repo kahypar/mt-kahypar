36cdc320166e0629c74211f85e2f6ad6197f9ede
Emil Hartmann <Emil03hartmann@gmail.com>
2025-12-10 15:22:51 +0100

    random constraint order

--- tools/constraint_generator.cc
+++ tools/constraint_generator.cc
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-
+#include <random>
 
 #include "mt-kahypar/definitions.h"
 #include "mt-kahypar/io/hypergraph_io.h"
@@ -37,6 +37,20 @@ std::optional<fs::path> findFileWithPrefix(const fs::path& path, const std::stri
         }
     }
     return std::nullopt;
+}
+
+void print_constraints(fs::path constraints_path, 
+                        vec<std::pair<HypernodeID, HypernodeID>>& constraint_list, 
+                        HypernodeID num_constraints) {
+    std::mt19937 gen(12345);
+    std::shuffle(constraint_list.begin(), constraint_list.end(), gen);
+
+    std::ofstream out_stream(constraints_path);
+    for (HypernodeID i = 0; i < num_constraints; i++) {
+        std::pair<HypernodeID, HypernodeID> constraint = constraint_list[i];
+        out_stream << constraint.first << " " << constraint.second << std::endl;
+    }
+    out_stream.close();
 }
 
 HypernodeID get_constraints(vec<vec<NodeID>>& adjacency, 
@@ -72,8 +86,8 @@ HypernodeID get_constraints(vec<vec<NodeID>>& adjacency,
     return constraint_count;
 }
 
-HypernodeID generate_constraints_from_hg(const fs::path hg_path, 
-                                            std::ofstream& out_stream, 
+HypernodeID generate_constraints_from_hg(const fs::path hg_path,
+                                            const fs::path constraints_path,
                                             HypernodeID num_constraints, 
                                             const HypernodeID max_constraints_per_node) {
     // Read Hypergraph
@@ -96,6 +110,7 @@ HypernodeID generate_constraints_from_hg(const fs::path hg_path,
     vec<vec<NodeID>> adjacency;
     adjacency.resize(num_nodes);
     HypernodeID generated_constraints = get_constraints(adjacency, hyperedges, max_constraints_per_node, num_constraints);
+    std::ofstream out_stream(constraints_path);
     for (NodeID i = 0; i < num_nodes; i++) {
         vec<NodeID> neighbors = adjacency[i];
         if(!neighbors.empty()) {
@@ -106,12 +121,13 @@ HypernodeID generate_constraints_from_hg(const fs::path hg_path,
         }
         
     }
+    out_stream.close();
     return generated_constraints;
 }
 
 HypernodeID generate_constraints_from_partitioned_hg(const fs::path hg_path, 
                                                         const fs::path part_hg_path, 
-                                                        std::ofstream& out_stream, 
+                                                        const fs::path constraints_path, 
                                                         HypernodeID num_constraints, 
                                                         const HypernodeID max_constraints_per_node) {
     HyperedgeID num_edges;
@@ -124,14 +140,13 @@ HypernodeID generate_constraints_from_partitioned_hg(const fs::path hg_path,
         num_constraints = num_nodes * DEFAULT_CONSTRAINT_FRACTION;
     }
 
+    vec<std::pair<HypernodeID, HypernodeID>> constraint_list;
+    constraint_list.reserve(num_nodes * max_constraints_per_node);
     HypernodeID constraint_count = 0;
     std::unordered_map<HypernodeID, HypernodeID> constraints_per_node;
     for (HypernodeID node = 0; node < num_nodes; node++) {
         PartitionID node_partition = partitions[node];
         for (HypernodeID other_node = node + 1; other_node < num_nodes; other_node++) {
-            if (constraint_count >= num_constraints){
-                    return constraint_count;
-                }
             if (constraints_per_node[node] >= max_constraints_per_node) break;
             if (constraints_per_node[other_node] >= max_constraints_per_node) continue;
 
@@ -139,12 +154,14 @@ HypernodeID generate_constraints_from_partitioned_hg(const fs::path hg_path,
                 constraint_count++;
                 constraints_per_node[node]++;
                 constraints_per_node[other_node]++;
-                out_stream << node << " " << other_node << std::endl; //maybe add constraint to list and pick random, but very expensive
+                constraint_list.push_back(std::make_pair(node, other_node));
             }
         }
         constraints_per_node.erase(node);
     }
-    return constraint_count;
+    num_constraints = std::min(num_constraints, constraint_count);
+    print_constraints(constraints_path, constraint_list, num_constraints);
+    return num_constraints;
 }
 
 int main(int argc, char* argv[]) {
@@ -208,7 +225,6 @@ int main(int argc, char* argv[]) {
 
     for (const auto& hg_file : hypergraph_files) {
         fs::path constraint_file = constraint_dir / (hg_file.filename().string() + "." + std::to_string(k) + CONSTRAINT_FILE_EXTENSION);
-        std::ofstream out_stream(constraint_file);
         HypernodeID generated_constraints;
         if (partitioned_hypergraph_path) {
             auto part_hg_file = findFileWithPrefix(partitioned_hypergraph_path.value(), hg_file.filename().string());
@@ -219,16 +235,13 @@ int main(int argc, char* argv[]) {
                     std::make_error_code(std::errc::not_a_directory)
                 );
             }
-            generated_constraints = generate_constraints_from_partitioned_hg(hg_file, part_hg_file.value(), out_stream, num_constraints, max_constraints_per_node);
+            generated_constraints = generate_constraints_from_partitioned_hg(hg_file, part_hg_file.value(), constraint_file, num_constraints, max_constraints_per_node);
         } else {
-            generated_constraints = generate_constraints_from_hg(hg_file, out_stream, num_constraints, max_constraints_per_node);
+            generated_constraints = generate_constraints_from_hg(hg_file, constraint_file, num_constraints, max_constraints_per_node);
         }
-        out_stream.close();
-
         LOG << "";
         LOG << "Generated " << generated_constraints << "constraints for hg:" << hg_file.filename();
-        LOG << "";
     }
-    
+    LOG << "";
     return 0;
 }
