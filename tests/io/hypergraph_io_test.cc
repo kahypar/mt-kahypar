b6bbca5ca27f5708eedfb604d0feeb952fb9c38a
Nikolai Maas <nikolai.maas@kit.edu>
2026-03-06 13:05:22 +0100

    new I/O tests #1

--- tests/io/hypergraph_io_test.cc
+++ tests/io/hypergraph_io_test.cc
@@ -66,7 +66,7 @@ class AnInputReader : public Test {
       const std::set<HypernodeID>& reference = references[he];
       size_t count = 0;
       for (const HypernodeID& pin : hypergraph.pins(he)) {
-        ASSERT_TRUE(reference.find(pin) != reference.end()) << V(he) << V(pin);
+        ASSERT_TRUE(reference.find(pin) != reference.end()) << V(he) << " " << V(pin);
         count++;
       }
       ASSERT_EQ(count, reference.size());
@@ -82,7 +82,7 @@ class AnInputReader : public Test {
         ASSERT_EQ(hypergraph.edgeSize(he), 2);
         for ( const HypernodeID& pin : hypergraph.pins(he) ) {
           if ( pin != hn ) {
-            ASSERT_TRUE(reference.find(pin) != reference.end()) << V(he) << V(pin);
+            ASSERT_TRUE(reference.find(pin) != reference.end()) << V(he) << " " << V(pin);
             ++cnt;
           }
         }
@@ -354,6 +354,100 @@ TYPED_TEST(AGraphReader, ReadsAMetisGraphWithNodeAndEdgeWeights) {
   ASSERT_EQ(6, this->hypergraph.nodeWeight(5));
   ASSERT_EQ(2, this->hypergraph.nodeWeight(6));
   ASSERT_EQ(1, this->hypergraph.nodeWeight(7));
+}
+
+TYPED_TEST(AGraphReader, MetisGraphWithMissingNewline) {
+  this->readHypergraph("../tests/instances/graph_no_newline.graph", FileFormat::Metis);
+
+  // Verify Neighbors
+  this->verifyNeighbors(
+    { { 1, 2, 4 },
+      { 0, 2, 3 },
+      { 0, 1, 3, 4 },
+      { 1, 2, 5, 6 },
+      { 0, 2, 5 },
+      { 3, 4, 6 },
+      { 3, 5 } } );
+
+  // Verify Node Weights
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(0));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(1));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(2));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(3));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(4));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(5));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(6));
+
+  // Verify Edge Weights
+  for ( HyperedgeID e : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ) {
+    ASSERT_EQ(1, this->hypergraph.edgeWeight(e));
+  }
+}
+
+TYPED_TEST(AGraphReader, MetisGraphWithMissingNewlineWindow) {
+  this->readHypergraph("../tests/instances/graph_no_newline_windows.graph", FileFormat::Metis);
+
+  // Verify Neighbors
+  this->verifyNeighbors(
+    { { 1, 2, 4 },
+      { 0, 2, 3 },
+      { 0, 1, 3, 4 },
+      { 1, 2, 5, 6 },
+      { 0, 2, 5 },
+      { 3, 4, 6 },
+      { 3, 5 } } );
+
+  // Verify Node Weights
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(0));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(1));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(2));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(3));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(4));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(5));
+  ASSERT_EQ(1, this->hypergraph.nodeWeight(6));
+
+  // Verify Edge Weights
+  for ( HyperedgeID e : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ) {
+    ASSERT_EQ(1, this->hypergraph.edgeWeight(e));
+  }
+}
+
+TYPED_TEST(AGraphReader, MetisGraphBadUnrolling) {
+  // trigger an edge case in the loop unrolling through combination of certain line length and missing newline
+  this->readHypergraph("../tests/instances/graph_bad_unrolling.graph", FileFormat::Metis);
+
+  // Verify Neighbors
+  this->verifyNeighbors(
+    { { 1, 2, 4 },
+      { 0, 2, 4 },
+      { 0, 1, 4 },
+      { },
+      { 0, 1, 2 },
+      { } } );
+}
+
+TYPED_TEST(AGraphReader, MetisGraphEmptyLines) {
+  this->readHypergraph("../tests/instances/graph_empty_lines.graph", FileFormat::Metis);
+
+  // Verify Neighbors
+  this->verifyNeighbors(
+    { { 1, 2 }, { 0, 2 }, { 0, 1 },
+      { }, { }, { },
+      { }, { }, { },
+      { }, { }, { },
+      { }, { }, { } } );
+}
+
+TYPED_TEST(AGraphReader, MetisTooFewLines) {
+  ASSERT_THROW(this->readHypergraph("../tests/instances/graph_too_few_lines.graph", FileFormat::Metis), InvalidInputException);
+}
+
+TYPED_TEST(AGraphReader, MetisTooFewLinesWindows) {
+  ASSERT_THROW(this->readHypergraph("../tests/instances/graph_too_few_lines_windows.graph", FileFormat::Metis), InvalidInputException);
+}
+
+TYPED_TEST(AGraphReader, MetisOneLineTooFew) {
+  ASSERT_THROW(this->readHypergraph("../tests/instances/graph_one_line_too_few.graph", FileFormat::Metis), InvalidInputException);
 }
 
 }  // namespace io
