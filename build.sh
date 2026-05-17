29c688fec3f5a3a34809164ba4fccd4267ae78ae
simeon <ucfoh@student.kit.edu>
2026-05-17 10:21:12 +0200

    build.sh update

--- build.sh
+++ build.sh
@@ -16,20 +16,12 @@ CMAKE_COMMANDS=$1
 mkdir -p build_debug
 mkdir -p build
 
-# Enable tests (keep both flags; one of them will match your CMake setup)
+# Enable tests
 TEST_FLAGS="-DKAHYPAR_ENABLE_TESTING=ON"
 
 cd build_debug && cmake .. -DCMAKE_BUILD_TYPE=Debug ${TEST_FLAGS} ${CMAKE_COMMANDS} && cd "${ROOT}"
 cmake --build build_debug --parallel "$(get_num_cores)" --target MtKaHyPar
 cmake --build build_debug --parallel "$(get_num_cores)" --target mtkahypar_tests
 
-cd build && cmake .. -DCMAKE_BUILD_TYPE=Release ${TEST_FLAGS} ${CMAKE_COMMANDS} && cd "${ROOT}"
+cd build && cmake .. -DCMAKE_BUILD_TYPE=Release ${CMAKE_COMMANDS} && cd "${ROOT}"
 cmake --build build --parallel "$(get_num_cores)" --target MtKaHyPar
-cmake --build build --parallel "$(get_num_cores)" --target mtkahypar_tests
-
-#cd build_debug && cmake .. -DCMAKE_BUILD_TYPE=Debug $CMAKE_COMMANDS && cd ${ROOT}
-#cmake  --build build_debug --parallel "$(get_num_cores)" --target MtKaHyPar
-#cd build && cmake .. -DCMAKE_BUILD_TYPE=Release $CMAKE_COMMANDS && cd ${ROOT}
-#cmake  --build build --parallel "$(get_num_cores)" --target MtKaHyPar
-#cd build && cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo $CMAKE_COMMANDS && cd ${ROOT}
-#cmake  --build build --parallel "$(get_num_cores)" --target MtKaHyPar
