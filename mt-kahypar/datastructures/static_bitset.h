e92e27e482f88f8d15a9d6e314243864fe59dc0b
Tobias Merkel <stylogey@gmail.com>
2026-07-29 11:01:33 +0200

    const cast members which are atomically read
    clang on MacOS does not like std::atomic_ref on const types

--- mt-kahypar/datastructures/static_bitset.h
+++ mt-kahypar/datastructures/static_bitset.h
@@ -114,7 +114,8 @@ class StaticBitset {
 
     MT_KAHYPAR_ATTRIBUTE_ALWAYS_INLINE Block loadCurrentBlock() {
       ASSERT(static_cast<size_t>(_current_block_id >> DIV_SHIFT) <= _num_blocks);
-      return std::atomic_ref(_bitset[_current_block_id >> DIV_SHIFT]).load(std::memory_order::relaxed);
+      return std::atomic_ref(const_cast<Block&>(_bitset[_current_block_id >> DIV_SHIFT]))
+          .load(std::memory_order::relaxed);
     }
 
     ENABLE_ASSERTIONS(const size_t _num_blocks;)
@@ -172,7 +173,7 @@ class StaticBitset {
   int popcount() const {
     int cnt = 0;
     for ( size_t i = 0; i < _num_blocks; ++i ) {
-      cnt += utils::popcount_64(std::atomic_ref(_bitset[i]).load(std::memory_order::relaxed));
+      cnt += utils::popcount_64(std::atomic_ref(const_cast<Block&>(_bitset[i])).load(std::memory_order::relaxed));
     }
     return cnt;
   }
