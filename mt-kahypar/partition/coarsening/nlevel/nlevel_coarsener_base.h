3c6978020117c9753614f98b9c032809f069b0f2
Nikolai Maas <nikolai.maas@kit.edu>
2026-07-31 17:29:50 +0200

    unrelated: remove unnecessary include

--- mt-kahypar/partition/coarsening/nlevel/nlevel_coarsener_base.h
+++ mt-kahypar/partition/coarsening/nlevel/nlevel_coarsener_base.h
@@ -31,7 +31,6 @@
 #include "mt-kahypar/partition/context.h"
 #include "mt-kahypar/partition/metrics.h"
 #include "mt-kahypar/partition/refinement/i_refiner.h"
-#include "mt-kahypar/partition/refinement/flows/i_flow_refiner.h"
 #include "mt-kahypar/parallel/stl/scalable_vector.h"
 #include "mt-kahypar/utils/utilities.h"
 #include "mt-kahypar/partition/coarsening/coarsening_commons.h"
